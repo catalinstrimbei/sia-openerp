5dd3cf3e3ad345734f1adb5309629d8ba49363b2
catalin.strimbei@gmail.com <catalin.strimbei@gmail.com@01a5fee1-9287-8e58-6871-dad33e70e5c6>
2011-11-23 10:57:50 +0000

    Upload PROMAN

---
+++ trunk/src/org/open/erp/services/proman/Responsabil.java
@@ -0,0 +1,13 @@
+package org.open.erp.services.proman;
+
+import org.open.erp.services.nomgen.Persoana;
+/**
+ * 
+ * @author catalin.strimbei
+ * 
+ * @BusinessObject(Entity)
+ * 
+ */
+public class Responsabil extends Persoana {
+
+}
