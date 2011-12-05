641a4ea85c1f793231de711b921cc98bbf4378cf
Narcisa.vengheac@gmail.com <Narcisa.vengheac@gmail.com@01a5fee1-9287-8e58-6871-dad33e70e5c6>
2011-12-05 23:50:38 +0000

    //OpenERP_PERSONAL narcisa.vengheac

--- trunk/OpenERP/OpenERP_PERSONAL/src/org/open/erp/services/personal/ActivitateTeamBuilding.java
+++ trunk/OpenERP/OpenERP_PERSONAL/src/org/open/erp/services/personal/ActivitateTeamBuilding.java
@@ -1,8 +1,6 @@
 package org.open.erp.services.personal;
 
-import java.util.Calendar;
 import java.util.List;
-//import org.open.erp.services.buget.Buget;
 
 
 /**
@@ -15,4 +13,40 @@ public class ActivitateTeamBuilding extends Activitate{
 	private Integer 	nrInscrisi;
 	private String		status;	
 	private List<Angajat> responsabili;
+	
+	
+	public Integer getNrInscrisi() {
+		return nrInscrisi;
+	}
+	public void setNrInscrisi(Integer nrInscrisi) {
+		this.nrInscrisi = nrInscrisi;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	public List<Angajat> getResponsabili() {
+		return responsabili;
+	}
+	public void setResponsabili(List<Angajat> responsabili) {
+		this.responsabili = responsabili;
+	}
+	public ActivitateTeamBuilding(Integer nrInscrisi, String status,
+			List<Angajat> responsabili) {
+		super();
+		this.nrInscrisi = nrInscrisi;
+		this.status = status;
+		this.responsabili = responsabili;
+	}
+	/**
+	 * 
+	 */
+	public ActivitateTeamBuilding() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	
 }
--- trunk/OpenERP/OpenERP_PERSONAL/src/org/open/erp/services/personal/ActivitateTraining.java
+++ trunk/OpenERP/OpenERP_PERSONAL/src/org/open/erp/services/personal/ActivitateTraining.java
@@ -10,4 +10,23 @@ import java.util.List;
 public class ActivitateTraining extends Activitate{
 	//private ProbaEvaluare				probaEvaluare;
 	private List<InstructorTraining>	instructori;
+
+	public List<InstructorTraining> getInstructori() {
+		return instructori;
+	}
+
+	public void setInstructori(List<InstructorTraining> instructori) {
+		this.instructori = instructori;
+	}
+
+	public ActivitateTraining(List<InstructorTraining> instructori) {
+		super();
+		this.instructori = instructori;
+	}
+
+	public ActivitateTraining() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
 }
