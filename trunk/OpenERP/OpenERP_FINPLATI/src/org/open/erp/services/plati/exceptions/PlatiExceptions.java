e6197a81a969602686f6322190fed471a8e6fe62
adrian.ilie.28@gmail.com <adrian.ilie.28@gmail.com@01a5fee1-9287-8e58-6871-dad33e70e5c6>
2011-12-06 10:31:12 +0000

    exceptii

---
+++ trunk/OpenERP/OpenERP_FINPLATI/src/org/open/erp/services/plati/exceptions/PlatiExceptions.java
@@ -0,0 +1,20 @@
+package org.open.erp.services.plati.exceptions;
+
+public class PlatiExceptions extends Exception{
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public PlatiExceptions() {
+    }
+	
+	public PlatiExceptions(String message, Throwable cause) {
+        super(message, cause);
+	}
+
+	public PlatiExceptions(String message) {
+        super(message);
+	}
+}
