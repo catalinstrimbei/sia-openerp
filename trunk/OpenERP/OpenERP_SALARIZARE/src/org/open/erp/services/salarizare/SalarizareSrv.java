bbdfee3788add9ddfcf56fb92979cd318fafb697
ionut.hrubaru@gmail.com <ionut.hrubaru@gmail.com@01a5fee1-9287-8e58-6871-dad33e70e5c6>
2011-12-08 20:33:04 +0000

    SALARIZARE - Ionut Hrubaru - actualizare doc java @UseCase

--- trunk/OpenERP/OpenERP_SALARIZARE/src/org/open/erp/services/salarizare/SalarizareSrv.java
+++ trunk/OpenERP/OpenERP_SALARIZARE/src/org/open/erp/services/salarizare/SalarizareSrv.java
@@ -16,13 +16,37 @@ import org.open.erp.services.personal.Angajat;
  * 
  * @EntitatiPersonalSrv: Angajat
  * 
- * @EntitatiLocale: Pontaj, Retinere, Spor, StatSalarii, ConfigurareProcente
+ * @EntitatiLocale: Pontaj, Retinere, Spor, Configurare, StatSalarii, CentralizatorStatSalarii 
  * 
  * @UseCase("inregistrare pontaj"):
  * 1. Creaza instanta pontaj
- * 2. Inregistrare pontaj
+ * 2. Inregistrare pontaj pe angajat
  * *. Returneata pontaj initializat pentru un angajat
- *  
+ * 
+ * @UseCase("inregistrare pontaj la nivel de luna"):
+ * 1. Creaza instanta pontaj pe fiecare angajat
+ * 2. Incarcare angajati cu contract activ
+ * 3. Inregistrare pontaj pe angajat pentru o anumita luna 
+ * 
+ * @UseCase("Adaugare ore suplimentare si de concediu"):
+ * 1.Incarcare pontaj angajat
+ * 2.Editare manuala a pontajului 
+ * 3.Adaugare ore suplimentare si de concediu
+ * 
+ * @UseCase("Inregistrare stat salarii"):
+ * 1.Incarcare lista pontaje
+ * 2.Calcul sporuri
+ * 3.Calcul venit brut
+ * 3.Calcul retineri 
+ * 4.Calcul Deduceri
+ * 5.Calcul Impozit
+ * 6.Calcul Venit Net
+ * 7.Generare stat plata
+ * 
+ * @UseCase("Generare centralizator stat salarii")
+ *  1.Incarcare calcule luna
+ *  2.Agregare sume
+ *  3.Generare totaluri pentru contabilitate
  * @UseCase("inregistrare pontaj"):
  *  
  */
