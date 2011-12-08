f2d28138cd84fac0285cbac84811c0fbeb2f381a
adrian.ilie.28@gmail.com <adrian.ilie.28@gmail.com@01a5fee1-9287-8e58-6871-dad33e70e5c6>
2011-12-08 21:24:27 +0000

    changes

--- trunk/OpenERP/OpenERP_FINPLATI/src/org/open/erp/services/plati/DocumentPlata.java
+++ trunk/OpenERP/OpenERP_FINPLATI/src/org/open/erp/services/plati/DocumentPlata.java
@@ -2,42 +2,20 @@ package org.open.erp.services.plati;
 
 import java.util.Date;
 
+import org.open.erp.services.nomgen.PersoanaJuridica;
+
 public class DocumentPlata {
 	private Integer idDocumentPlata;
-	private String tip;
-	private Date data;
-	private String seria;
-	private Integer nr;
+
+	public DocumentPlata(Integer idDocumentPlata){
+		this.idDocumentPlata = idDocumentPlata;
+	}
 	
 	public Integer getIdDocumentPlata() {
 		return idDocumentPlata;
 	}
+
 	public void setIdDocumentPlata(Integer idDocumentPlata) {
 		this.idDocumentPlata = idDocumentPlata;
 	}
-	public String getTip() {
-		return tip;
-	}
-	public void setTip(String tip) {
-		this.tip = tip;
-	}
-	public Date getData() {
-		return data;
-	}
-	public void setData(Date data) {
-		this.data = data;
-	}
-	public String getSeria() {
-		return seria;
-	}
-	public void setSeria(String seria) {
-		this.seria = seria;
-	}
-	public Integer getNr() {
-		return nr;
-	}
-	public void setNr(Integer nr) {
-		this.nr = nr;
-	}
-	
 }
