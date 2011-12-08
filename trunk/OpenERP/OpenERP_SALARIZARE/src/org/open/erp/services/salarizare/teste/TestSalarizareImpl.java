d09736a0165f3b5b954a8797bf5529ef88f5575d
ionut.hrubaru@gmail.com <ionut.hrubaru@gmail.com@01a5fee1-9287-8e58-6871-dad33e70e5c6>
2011-12-08 20:33:40 +0000

    SALARIZARE - Ionut Hrubaru - adaugare teste mai rafinate

--- trunk/OpenERP/OpenERP_SALARIZARE/src/org/open/erp/services/salarizare/teste/TestSalarizareImpl.java
+++ trunk/OpenERP/OpenERP_SALARIZARE/src/org/open/erp/services/salarizare/teste/TestSalarizareImpl.java
@@ -2,6 +2,8 @@ package org.open.erp.services.salarizare.teste;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -47,9 +49,109 @@ public class TestSalarizareImpl {
 	}
 	
 	@Test
+	public void calculSporuriAngajati() {
+		logger.info("Begin test: calculSporuriAngajat");
+		ArrayList<Angajat> angajati= new ArrayList<Angajat>();
+		angajati.addAll(personalSrvInstance.getListaAngajati());
+		
+		//parcurgem si apelam calculele pt fiecare angajat dupa care salvam in DB
+		for (Angajat angajat:angajati){
+		salarizareSrvInstance.calculSporuriAngajat(2011, 11, angajat);
+		}
+		logger.info("End test: calculSporuriAngajat");
+	}
+	
+	@Test
+	public void calculRetineriAngajati() {
+		logger.info("Begin test: calculRetineriAngajat");
+		ArrayList<Angajat> angajati= new ArrayList<Angajat>();
+		angajati.addAll(personalSrvInstance.getListaAngajati());
+		
+		//parcurgem si apelam calculele pt fiecare angajat dupa care salvam in DB
+		for (Angajat angajat:angajati){
+		salarizareSrvInstance.calculRetineriAngajat(2011, 11, angajat);
+		}
+		logger.info("End test: calculRetineriAngajat");
+	}
+	
+	@Test
+	public void calculVenitBrutAngajati() {
+		logger.info("Begin test: calculVenitBrutAngajat");
+		ArrayList<Angajat> angajati= new ArrayList<Angajat>();
+		angajati.addAll(personalSrvInstance.getListaAngajati());
+		
+		//parcurgem si apelam calculele pt fiecare angajat dupa care salvam in DB
+		for (Angajat angajat:angajati){
+		salarizareSrvInstance.calculVenitBrut(2011, 11, angajat);
+		}
+		logger.info("End test: calculVenitBrutAngajat");
+	}
+	
+	@Test
+	public void calculRetineriObligatoriiAngajati() {
+		logger.info("Begin test: calculRetineriObligatoriiAngajat");
+		ArrayList<Angajat> angajati= new ArrayList<Angajat>();
+		angajati.addAll(personalSrvInstance.getListaAngajati());
+		
+		//parcurgem si apelam calculele pt fiecare angajat dupa care salvam in DB
+		for (Angajat angajat:angajati){
+		salarizareSrvInstance.calculRetineriObligatorii(2011, 11, angajat,"CAS");
+		salarizareSrvInstance.calculRetineriObligatorii(2011, 11, angajat,"CASS");
+		salarizareSrvInstance.calculRetineriObligatorii(2011, 11, angajat,"SOMAJ");		
+		}
+		logger.info("End test: calculRetineriObligatoriiAngajat");
+	}
+	
+	@Test
+	public void calculImpozitAngajati() {
+		logger.info("Begin test: calculImpozitAngajat");
+		ArrayList<Angajat> angajati= new ArrayList<Angajat>();
+		angajati.addAll(personalSrvInstance.getListaAngajati());
+		
+		//parcurgem si apelam calculele pt fiecare angajat dupa care salvam in DB
+		for (Angajat angajat:angajati){
+		salarizareSrvInstance.calculImpozit(2011, 11, angajat);
+		}
+		logger.info("End test: calculImpozitAngajat");
+	}
+	
+	@Test
+	public void calculDeduceriAngajati() {
+		logger.info("Begin test: calculDeduceriAngajat");
+		ArrayList<Angajat> angajati= new ArrayList<Angajat>();
+		angajati.addAll(personalSrvInstance.getListaAngajati());
+		
+		//parcurgem si apelam calculele pt fiecare angajat dupa care salvam in DB
+		for (Angajat angajat:angajati){
+		salarizareSrvInstance.calculDeduceri(2011, 11, angajat);
+		}
+		logger.info("End test: calculDeduceriAngajat");
+	}
+	
+	@Test
 	public void testinregistrarStatSalariiLuna() {
 		logger.info("Begin test: inregistrareStatSalariiLuna");
 		salarizareSrvInstance.inregistrarStatSalariiLuna(2011, 11);
 		logger.info("End test: inregistrareStatSalariiLuna");
+	}
+	
+	@Test
+	public void testInregistrareSpor() {
+		logger.info("Begin test: inregistrareSpor");
+		Angajat angajat = personalSrvInstance.getAngajatById(1);
+		Spor spor = salarizareSrvInstance.inregistrareSpor("Bonus", 1, 2011, 11, angajat, 1, 100.0);
+		assertNotNull("Metoda de creere a pontajului nu a functionat!", spor);
+		
+		logger.info("End test: inregistrareSpor");
+	}
+	
+	@Test
+	public void testInregistrareRetinere() {
+		logger.info("Begin test: inregistrareRetinere");
+		Angajat angajat = personalSrvInstance.getAngajatById(1);
+		Retinere retinere = salarizareSrvInstance.inregistrareRetinere("Penalizare", 1, 2011, 11, angajat, 1, 100.0);
+		assertNotNull("Metoda de creere a pontajului nu a functionat!", retinere);
+		
+		logger.info("End test: inregistrareRetinere");
 	}
 }
