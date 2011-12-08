698fa3b7d7c8c9d73d75712a94e22eeff75dcc0c
adrian.ilie.28@gmail.com <adrian.ilie.28@gmail.com@01a5fee1-9287-8e58-6871-dad33e70e5c6>
2011-12-08 21:21:29 +0000

    changes

--- trunk/OpenERP/OpenERP_FINPLATI/src/org/open/erp/services/plati/CEC.java
+++ trunk/OpenERP/OpenERP_FINPLATI/src/org/open/erp/services/plati/CEC.java
@@ -1,39 +1,66 @@
 package org.open.erp.services.plati;
 
+import java.util.Date;
+
+import org.open.erp.services.nomgen.Material;
 import org.open.erp.services.nomgen.PersoanaJuridica;
 
-public class CEC extends DocumentPlata {
-	private Double suma;
+public class CEC extends DocumentPlata{
+	private String seriaNr;
+	private Date data;
+	private Double totalPlata;
 	private PersoanaJuridica emitent;
-	private PersoanaJuridica tragator; 
-	private Double sumaPlataAvans;
-	
-	public Double getSuma() {
-		return suma;
+	private PersoanaJuridica beneficiar;
+	private ContBancaPJ contEmitent;
+
+	public CEC(Integer idDocumentPlata, String seriaNr, Date data, Double totalPlata, PersoanaJuridica emitent, PersoanaJuridica beneficiar, ContBancaPJ contEmitent) {
+		super(idDocumentPlata);
+		this.setSeriaNr(seriaNr);
+		this.data = data;
+		this.totalPlata = totalPlata;
+		this.emitent = emitent;
+		this.beneficiar = beneficiar;
+		this.contEmitent = contEmitent;
 	}
 	
-	public void setSuma(Double suma) {
-		this.suma = suma;
+	public Date getData() {
+		return data;
 	}
-	
+	public void setData(Date data) {
+		this.data = data;
+	}
 	public PersoanaJuridica getEmitent() {
 		return emitent;
 	}
-	
-	public void setEmitent(PersoanaJuridica emitent){
+	public void setEmitent(PersoanaJuridica emitent) {
 		this.emitent = emitent;
 	}
-	
-	public PersoanaJuridica getTragator() {
-		return tragator;
+	public PersoanaJuridica getBeneficiar() {
+		return beneficiar;
 	}
-	
-	public void setTragator(PersoanaJuridica tragator){
-		this.tragator = tragator;
-		
+	public void setBeneficiar(PersoanaJuridica beneficiar) {
+		this.beneficiar = beneficiar;
 	}
-	public Double PlataAvans(Double suma, Double procentAvans) {
-		return sumaPlataAvans;
+	public Double getTotalPlata() {
+		return totalPlata;
 	}
-	
-}
+	public void setTotalPlata(Double totalPlata) {
+		this.totalPlata = totalPlata;
+	}
+
+	public ContBancaPJ getContEmitent() {
+		return contEmitent;
+	}
+
+	public void setContEmitent(ContBancaPJ contEmitent) {
+		this.contEmitent = contEmitent;
+	}
+
+	public String getSeriaNr() {
+		return seriaNr;
+	}
+
+	public void setSeriaNr(String seriaNr) {
+		this.seriaNr = seriaNr;
+	}
+}	
