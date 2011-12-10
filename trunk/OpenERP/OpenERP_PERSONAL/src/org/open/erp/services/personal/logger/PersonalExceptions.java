e5cb3ab112e36e83425bfa7ecd24e2429c95764b
Anatolie.Darii@gmail.com <Anatolie.Darii@gmail.com@01a5fee1-9287-8e58-6871-dad33e70e5c6>
2011-12-10 12:46:55 +0000

    PERSONAL :> Anatolie Darii -> Am sters un constructor din personal exceptions (se pare la la cei cu jre 6 dadea eroare)

--- trunk/OpenERP/OpenERP_PERSONAL/src/org/open/erp/services/personal/logger/PersonalExceptions.java
+++ trunk/OpenERP/OpenERP_PERSONAL/src/org/open/erp/services/personal/logger/PersonalExceptions.java
@@ -13,11 +13,6 @@ public class PersonalExceptions extends Exception  {
 		super();
 		// TODO Auto-generated constructor stub
 	}
-	public PersonalExceptions(String arg0, Throwable arg1, boolean arg2,
-			boolean arg3) {
-		super(arg0, arg1, arg2, arg3);
-		// TODO Auto-generated constructor stub
-	}
 	public PersonalExceptions(String arg0, Throwable arg1) {
 		super(arg0, arg1);
 		// TODO Auto-generated constructor stub
