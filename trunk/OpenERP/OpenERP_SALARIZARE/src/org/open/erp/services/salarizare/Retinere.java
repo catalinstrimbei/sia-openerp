60d004b90e34444815ac0a984c39ad4d7003d0af
ionut.hrubaru@gmail.com <ionut.hrubaru@gmail.com@01a5fee1-9287-8e58-6871-dad33e70e5c6>
2011-12-30 09:54:19 +0000

    SALARIZARE - Ionut Hrubaru - entitate JPA Retinere

--- trunk/OpenERP/OpenERP_SALARIZARE/src/org/open/erp/services/salarizare/Retinere.java
+++ trunk/OpenERP/OpenERP_SALARIZARE/src/org/open/erp/services/salarizare/Retinere.java
@@ -1,16 +1,35 @@
 package org.open.erp.services.salarizare;
 
-import org.open.erp.services.personal.Angajat;
+import javax.persistence.Entity;
 
+import org.open.erp.services.personal.Angajat;
+import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+import static javax.persistence.GenerationType.AUTO;
+import static javax.persistence.GenerationType.SEQUENCE;
+import javax.persistence.ManyToOne;
+
+@Entity
 public class Retinere {
+	@Id
+	@GeneratedValue(strategy = AUTO)
+	private Integer idRetinere;
 	private String denumire;
 	private Integer tip;//general sau la nivel de angajat
+	@ManyToOne
 	private Angajat angajat; //can be null
 	private Integer an;
 	private Integer luna;
 	private Integer modCalcul;//procent sau suma fixa
 	private Double valoare;//procent sau suma fixa
 	
+	
+	public Integer getIdRetinere() {
+		return idRetinere;
+	}
+	public void setIdRetinere(Integer idRetinere) {
+		this.idRetinere = idRetinere;
+	}
 	public Double getValoare() {
 		return valoare;
 	}
@@ -19,6 +38,20 @@ public class Retinere {
 	}
 	public Retinere() {
 		super();
+	}
+
+	public Retinere(Integer idRetinere, String denumire, Integer tip,
+			Angajat angajat, Integer an, Integer luna, Integer modCalcul,
+			Double valoare) {
+		super();
+		this.idRetinere = idRetinere;
+		this.denumire = denumire;
+		this.tip = tip;
+		this.angajat = angajat;
+		this.an = an;
+		this.luna = luna;
+		this.modCalcul = modCalcul;
+		this.valoare = valoare;
 	}
 	public Retinere(String denumire, Integer tip, Angajat angajat, Integer an,
 			Integer luna, Integer modCalcul, Double valoare) {
@@ -66,6 +99,30 @@ public class Retinere {
 	}
 	public void setModCalcul(Integer modCalcul) {
 		this.modCalcul = modCalcul;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result
+				+ ((idRetinere == null) ? 0 : idRetinere.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Retinere other = (Retinere) obj;
+		if (idRetinere == null) {
+			if (other.idRetinere != null)
+				return false;
+		} else if (!idRetinere.equals(other.idRetinere))
+			return false;
+		return true;
 	}
 	
 	
