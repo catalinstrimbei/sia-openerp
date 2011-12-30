1465acc28e87761fd9be9c2c46905f4b173daafd
ionut.hrubaru@gmail.com <ionut.hrubaru@gmail.com@01a5fee1-9287-8e58-6871-dad33e70e5c6>
2011-12-30 10:03:08 +0000

    SALARIZARE - Ionut Hrubaru - entitate JPA StatSalarii

--- trunk/OpenERP/OpenERP_SALARIZARE/src/org/open/erp/services/salarizare/StatSalarii.java
+++ trunk/OpenERP/OpenERP_SALARIZARE/src/org/open/erp/services/salarizare/StatSalarii.java
@@ -1,6 +1,18 @@
 package org.open.erp.services.salarizare;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+import javax.persistence.OneToOne;
+import javax.persistence.JoinColumn;
+
+@Entity
 public class StatSalarii {
+	@Id
+	@GeneratedValue
+	private Integer idStatSalarii;
+	@OneToOne
+	@JoinColumn(name = "pontaj_idPontaj", referencedColumnName = "idPontaj")
 	private Pontaj pontaj;
 	private Double cas;
 	private Double cass;
@@ -13,6 +25,15 @@ public class StatSalarii {
 	public StatSalarii() {
 		super();
 	}
+	
+	public Integer getIdStatSalarii() {
+		return idStatSalarii;
+	}
+
+	public void setIdStatSalarii(Integer idStatSalarii) {
+		this.idStatSalarii = idStatSalarii;
+	}
+
 	public Pontaj getPontaj() {
 		return pontaj;
 	}
@@ -66,6 +87,48 @@ public class StatSalarii {
 	}
 	public void setSalarNet(Double salarNet) {
 		this.salarNet = salarNet;
+	}
+
+	public StatSalarii(Integer idStatSalarii, Pontaj pontaj, Double cas,
+			Double cass, Double somaj, Double alteRetineri, Double alteSporuri,
+			Double salarBrut, Double impozit, Double salarNet) {
+		super();
+		this.idStatSalarii = idStatSalarii;
+		this.pontaj = pontaj;
+		this.cas = cas;
+		this.cass = cass;
+		this.somaj = somaj;
+		this.alteRetineri = alteRetineri;
+		this.alteSporuri = alteSporuri;
+		this.salarBrut = salarBrut;
+		this.impozit = impozit;
+		this.salarNet = salarNet;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result
+				+ ((idStatSalarii == null) ? 0 : idStatSalarii.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		StatSalarii other = (StatSalarii) obj;
+		if (idStatSalarii == null) {
+			if (other.idStatSalarii != null)
+				return false;
+		} else if (!idStatSalarii.equals(other.idStatSalarii))
+			return false;
+		return true;
 	}
 	
 }
