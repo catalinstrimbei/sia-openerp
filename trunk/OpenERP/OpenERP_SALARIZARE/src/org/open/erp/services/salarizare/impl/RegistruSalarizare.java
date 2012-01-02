a65dfbe0a8b4c6d94fa60eb2b6496b13848cadac
ionut.hrubaru@gmail.com <ionut.hrubaru@gmail.com@01a5fee1-9287-8e58-6871-dad33e70e5c6>
2012-01-02 10:20:05 +0000

    SALARIZARE - Ionut Hrubaru - RegistruSalarizare - metoda interogare getSporuriAngajat

--- trunk/OpenERP/OpenERP_SALARIZARE/src/org/open/erp/services/salarizare/impl/RegistruSalarizare.java
+++ trunk/OpenERP/OpenERP_SALARIZARE/src/org/open/erp/services/salarizare/impl/RegistruSalarizare.java
@@ -1,6 +1,7 @@
 package org.open.erp.services.salarizare.impl;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.EntityManager;
 
@@ -38,12 +39,19 @@ public class RegistruSalarizare {
 		return p;
 	}
 
-	public ArrayList<Spor> getSporuriAngajat(Integer an, Integer luna, Angajat angajat){
-		ArrayList<Spor> sporuri= new ArrayList<Spor>();
+	public List<Spor> getSporuriAngajat(Integer an, Integer luna, Angajat angajat){
+		List<Spor> sporuri= new ArrayList<Spor>();
 		//aici apelam ceva din DB care incarca sporurile
 		//sporuri.add(new Spor("Bonus", 1, 2011, 11, angajat, 1, 100.0));
 		
 		//sporuri.add(new Spor("Bonus procent", 2, 2011, 11, angajat, 2, 5.0));
+		sporuri = entityManager.createQuery("SELECT s FROM Spor s " +
+				"WHERE s.angajat.marca=:marca AND s.an=:an AND s.luna=:luna")
+				.setParameter("marca", angajat.getMarca())
+				.setParameter("an", an)
+				.setParameter("luna", luna)
+				.getResultList();
+		
 		return sporuri;
 	}
 
