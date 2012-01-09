94164e16350ec704e8bf92218126ba8110ec5a1b
andreea.anton88@gmail.com <andreea.anton88@gmail.com@01a5fee1-9287-8e58-6871-dad33e70e5c6>
2012-01-09 18:30:23 +0000

    Personal, Anton Andreea, modificare RegistruPersonalEJB

--- trunk/OpenERP/OpenERP_PERSONAL/src/org/open/erp/services/personal/impl/RegistruPersonalEJB.java
+++ trunk/OpenERP/OpenERP_PERSONAL/src/org/open/erp/services/personal/impl/RegistruPersonalEJB.java
@@ -1,5 +1,17 @@
 package org.open.erp.services.personal.impl;
 
+import javax.persistence.EntityManager;
+
+import org.open.erp.services.personal.logger.PersonalLogger;
+
 public class RegistruPersonalEJB {
+	private static PersonalLogger logger ;	
+
+	/* set up */
+	private EntityManager entityManager;
+	public RegistruPersonalEJB(EntityManager em) {
+		entityManager = em;
+	}
+
 
 }
