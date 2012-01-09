f5bb35ca24af90f72e6f0b0ff75802fddfcd3083
andreea.anton88@gmail.com <andreea.anton88@gmail.com@01a5fee1-9287-8e58-6871-dad33e70e5c6>
2012-01-09 22:03:59 +0000

    Personal, Anton Andreea, modificare RegistruPersonalEJB

--- trunk/OpenERP/OpenERP_PERSONAL/src/org/open/erp/services/personal/impl/RegistruPersonalEJB.java
+++ trunk/OpenERP/OpenERP_PERSONAL/src/org/open/erp/services/personal/impl/RegistruPersonalEJB.java
@@ -4,9 +4,13 @@ import java.util.Collection;
 
 import javax.persistence.EntityManager;
 
+import org.open.erp.services.personal.AnuntLocMunca;
+import org.open.erp.services.personal.CV;
+import org.open.erp.services.personal.ContractMunca;
 import org.open.erp.services.personal.Activitate;
 import org.open.erp.services.personal.DosarAngajat;
 import org.open.erp.services.personal.DummyDepartament;
+import org.open.erp.services.personal.Functie;
 import org.open.erp.services.personal.ProbaEvaluare;
 import org.open.erp.services.personal.RezultatProbaEvaluare;
 import org.open.erp.services.personal.logger.PersonalLogger;
@@ -23,9 +27,9 @@ public class RegistruPersonalEJB {
 	public Collection<ProbaEvaluare> getProbaEvaluarePeDepartament(DummyDepartament departament_) throws Exception{
 		try{
 			return entityManager.createQuery("SELECT pe FROM ProbeEvaluare pe " +
-											"WHERE pe.departament = :departament AND pe.scop = 'EvaluarePeriodica'")
-											.setParameter("departament", departament_).
-											getResultList();
+											"WHERE pe.Id = :idDepartament AND pe.scop = 'EvaluarePeriodica'")
+											.setParameter("idDepartament", departament_.getId())
+											.getResultList();
 		}catch(Exception ex){
 			logger.logINFO("EROARE PERSISTENTA ***** ");
 			ex.printStackTrace();
@@ -36,16 +40,42 @@ public class RegistruPersonalEJB {
 	public Collection<RezultatProbaEvaluare> getRezultateProbaEvaluare(ProbaEvaluare probaEvaluare_) throws Exception{
 		try{
 			return entityManager.createQuery("SELECT rpe FROM RezultatProbeEvaluare rpe " +
-											"WHERE rpe.probaEvaluare = :probaEvaluare")
-											.setParameter("probaEvaluare", probaEvaluare_).
-											getResultList();
+											"WHERE rpe.idProba = :probaEvaluare")
+											.setParameter("probaEvaluare", probaEvaluare_.getIdProba())
+											.getResultList();
 		}catch(Exception ex){
 			logger.logINFO("EROARE PERSISTENTA ***** ");
 			ex.printStackTrace();
 			throw ex;
 		}
 	}
+
 	
+	public Collection<CV> getCVuriPeAnuntLocMunca(AnuntLocMunca anuntMunca_) throws Exception{
+		try{
+			return entityManager.createQuery("SELECT cv FROM CV cv " +
+											"WHERE cv.idFunctie = :idFunctie")
+											.setParameter("idFunctie", anuntMunca_.getFunctie().getIdFunctie())
+											.getResultList();
+		}catch(Exception ex){
+			logger.logINFO("EROARE PERSISTENTA ***** ");
+			ex.printStackTrace();
+			throw ex;
+		}
+	}
+	public Collection<ContractMunca> getContracteMuncaPeFunctie(Functie functie_) throws Exception{
+		try{
+			return entityManager.createQuery("SELECT cm FROM ContractMunca cm " +
+											"WHERE cm.idFunctie = :idFunctie")
+											.setParameter("idFunctie", functie_.getIdFunctie())
+											.getResultList();
+		}catch(Exception ex){
+			logger.logINFO("EROARE PERSISTENTA ***** ");
+			ex.printStackTrace();
+			throw ex;
+		}
+	}
+
 	public Collection<Activitate> getActivitatiByEvenimentEJB(Integer IdEveniment) throws Exception	{
 		try
 		{
@@ -63,4 +93,6 @@ public class RegistruPersonalEJB {
 	}
 
 
+
 }
+
