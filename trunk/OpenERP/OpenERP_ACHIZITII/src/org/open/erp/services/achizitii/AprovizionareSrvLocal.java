c4cbef96c5a3f55e518ee536ca7c39bc36881dce
delia.schiopu@gmail.com <delia.schiopu@gmail.com@01a5fee1-9287-8e58-6871-dad33e70e5c6>
2012-01-09 19:26:05 +0000

    erpAchizitii - Delia Schiopu - creare interfete Local, Remote

---
+++ trunk/OpenERP/OpenERP_ACHIZITII/src/org/open/erp/services/achizitii/AprovizionareSrvLocal.java
@@ -0,0 +1,8 @@
+package org.open.erp.services.achizitii;
+
+import javax.ejb.Local;
+
+@Local
+public interface AprovizionareSrvLocal extends AprovizionareSrv {
+
+}
---
+++ trunk/OpenERP/OpenERP_ACHIZITII/src/org/open/erp/services/achizitii/AprovizionareSrvRemote.java
@@ -0,0 +1,8 @@
+package org.open.erp.services.achizitii;
+
+import javax.ejb.Remote;
+
+@Remote
+public interface AprovizionareSrvRemote extends AprovizionareSrv{
+
+}
