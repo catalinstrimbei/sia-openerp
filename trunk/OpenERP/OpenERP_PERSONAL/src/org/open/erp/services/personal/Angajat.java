15a1e8a705264a019418284e1a1623976dca7142
Anatolie.Darii@gmail.com <Anatolie.Darii@gmail.com@01a5fee1-9287-8e58-6871-dad33e70e5c6>
2012-01-10 22:40:57 +0000

    OpenERP_Personal - Anatolie Darii - metoda angajareEJB

--- trunk/OpenERP/OpenERP_PERSONAL/src/org/open/erp/services/personal/Angajat.java
+++ trunk/OpenERP/OpenERP_PERSONAL/src/org/open/erp/services/personal/Angajat.java
@@ -7,6 +7,7 @@ import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.ManyToMany;
 import javax.persistence.OneToMany;
 /**
@@ -20,6 +21,7 @@ public class Angajat extends Candidat implements Serializable{
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+	
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Integer			marca;	
 	private Boolean			activ;
--- trunk/OpenERP/OpenERP_PERSONAL/src/org/open/erp/services/personal/DummyPersoana.java
+++ trunk/OpenERP/OpenERP_PERSONAL/src/org/open/erp/services/personal/DummyPersoana.java
@@ -3,6 +3,8 @@ package org.open.erp.services.personal;
 import java.io.Serializable;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Inheritance;
 import javax.persistence.InheritanceType;
@@ -19,7 +21,7 @@ public class DummyPersoana implements Serializable{
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	@Id
+	@Id@GeneratedValue(strategy = GenerationType.AUTO)
 	private Integer id;
 	private String  adresa;
 	
