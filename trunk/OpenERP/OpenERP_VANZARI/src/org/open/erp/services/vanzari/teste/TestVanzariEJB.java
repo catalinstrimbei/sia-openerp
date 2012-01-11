cc081c1cb19417a8652138437445a86dfa114056
irinambogdan@gmail.com <irinambogdan@gmail.com@01a5fee1-9287-8e58-6871-dad33e70e5c6>
2012-01-11 06:04:03 +0000

    Vanzari - Irina Bogdan - teste

--- trunk/OpenERP/OpenERP_VANZARI/src/org/open/erp/services/vanzari/teste/TestVanzariEJB.java
+++ trunk/OpenERP/OpenERP_VANZARI/src/org/open/erp/services/vanzari/teste/TestVanzariEJB.java
@@ -1,5 +1,9 @@
 package org.open.erp.services.vanzari.teste;
 
+import static org.junit.Assert.assertNotNull;
+
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.Properties;
 
 import javax.naming.InitialContext;
@@ -9,8 +13,15 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 import org.open.erp.services.ctbgen.ContabilizareSrv;
 import org.open.erp.services.nomgen.NomenclatoareSrv;
+import org.open.erp.services.nomgen.Produs;
 import org.open.erp.services.stocuri.StocuriSrv;
+import org.open.erp.services.vanzari.Client;
+import org.open.erp.services.vanzari.Comanda;
+import org.open.erp.services.vanzari.FacturaEmisa;
+import org.open.erp.services.vanzari.LinieComanda;
+import org.open.erp.services.vanzari.LinieFacturaEmisa;
 import org.open.erp.services.vanzari.VanzariSrv;
+import org.open.erp.services.vanzari.Vanzator;
 import org.open.erp.services.vanzari.impl.VanzariImpl;
 
 public class TestVanzariEJB {
@@ -42,7 +53,61 @@ public class TestVanzariEJB {
 	}
 	
 	@Test
-	public void testinregistrareComanda(){
-		//vanzariInstance
+	public void testinregistrareComanda() {
+		Produs[] p = new Produs[2];
+		p[0] = new Produs(1, "lapte batut", "buc", new Date(), 10, (float)0.24, 3.2); 
+		p[1] = new Produs();
+		Double cant[] = {2.0, 1.0};
+		Client client = new Client(1, 1, 2, "Gigel", "gigel@yahoo.com", "0987654321", "CT123456", 0.0);
+		Comanda comanda = vanzariInstance.inregistrareComanda(p, cant, client);
+		assertNotNull("Nu exista comanda!", comanda);
 	}
+	
+	@Test
+	public void testfacturareProduse(){
+		Client client = new Client(1, 1, 2, "Gigel", "gigel@yahoo.com", "0987654321", "CT123456", 0.0);
+		Produs produs = new Produs(1, "lapte batut", "buc", new Date(), 10, (float)0.24, 3.2);
+		Comanda comanda = new Comanda(1, new Date(), client, Comanda.PENDING);
+		//LinieComanda
+		ArrayList<LinieComanda> lc = new ArrayList<LinieComanda>();
+		lc.add(new LinieComanda(produs, 10.0));
+		comanda.setProduseComandate(lc);
+		Vanzator vanz = new Vanzator();
+		FacturaEmisa fact = vanzariInstance.facturareProduse(comanda, client, vanz);
+		assertNotNull("Nu exista factura!", fact);
+	}
+	
+	@Test
+	public void testGetFacturiClient(){
+		Client client = new Client(1, 1, 2, "Gigel", "gigel@yahoo.com", "0987654321", "CT123456", 0.0);
+		assertNotNull("Nu exista facturi", vanzariInstance.getFacturiClient(client));
+	}
+	
+	@Test
+	public void testReturProduse(){
+		Client client = new Client(1, 1, 2, "Gigel", "gigel@yahoo.com", "0987654321", "CT123456", 0.0);
+		Vanzator vanzator = new Vanzator();
+		
+		FacturaEmisa fact = new FacturaEmisa(1, client, vanzator, FacturaEmisa.NEPLATITA);
+		
+		Produs produs = new Produs(1, "lapte batut", "buc", new Date(), 10, (float)0.24, 3.2);
+		LinieFacturaEmisa lf = new LinieFacturaEmisa(produs, 10.0);
+		
+		//fact.addLinie(lf);
+		fact.getProduseFacturate().add(lf);
+		
+		vanzariInstance.returProduse(fact);
+		
+		// how to test a method that returns void? thinking (in progress)..
+		
+	}
+
+	/*@Test
+	public void testinregistrareFactura(){
+		Client client = new Client(1, 1, 2, "Gigel", "gigel@yahoo.com", "0987654321", "CT123456", 0.0);
+		Vanzator vanzator = new Vanzator();
+		FacturaEmisa factura = new FacturaEmisa(1, client, vanzator, FacturaEmisa.NEPLATITA);
+		Integer result = vanzariInstance.inregistrareFactura(factura);
+		assertEquals("Nu s-a inregistrat in contabilitate", result.getClass().getSimpleName(),"Integer");
+	}*/
 }
