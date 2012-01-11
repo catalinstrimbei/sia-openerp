f9a484bd54a5176cefa025e0949041caa3e2c35d
bogdandcn@gmail.com <bogdandcn@gmail.com@01a5fee1-9287-8e58-6871-dad33e70e5c6>
2012-01-11 10:50:08 +0000

    add registruArticol

---
+++ trunk/OpenERP/OpenERP_ACHIZITII/src/org/open/erp/services/acizitii/registri/RegistruArticol.java
@@ -0,0 +1,31 @@
+package org.open.erp.services.acizitii.registri;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+
+import org.open.erp.services.achizitii.Articol;
+import org.open.erp.services.stocuri.registri.Registru;
+
+public class RegistruArticol extends Registru {
+
+	private static final String SQL_DEFAULT = "SELECT a FROM Articol a ";
+
+	public RegistruArticol(EntityManager em) {
+		super(em);
+
+	}
+
+	public List<Articol> getArticoleByCategorie(Integer id) {
+		return em.createNamedQuery("getArticoleByCategorie")
+				.setParameter("id", id).getResultList();
+	}
+	
+	@Override
+	public <T> List<T> getListaByClasa(Class<T> clasa) {
+		return em.createQuery(SQL_DEFAULT).getResultList();
+	}
+
+	
+	
+}
