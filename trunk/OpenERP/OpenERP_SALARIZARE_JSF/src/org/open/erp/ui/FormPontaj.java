eff680603a69504b05a6bab723648164f50a6f60
radu.makovei@gmail.com <radu.makovei@gmail.com@01a5fee1-9287-8e58-6871-dad33e70e5c6>
2012-01-23 11:06:23 +0000

    SALARIZARE - Radu Macovei - formPontaj modificari

--- trunk/OpenERP/OpenERP_SALARIZARE_JSF/src/org/open/erp/ui/FormPontaj.java
+++ trunk/OpenERP/OpenERP_SALARIZARE_JSF/src/org/open/erp/ui/FormPontaj.java
@@ -10,12 +10,13 @@ import javax.ejb.EJB;
 import javax.faces.component.UIComponent;
 import javax.faces.context.FacesContext;
 import javax.faces.convert.Converter;
+import javax.faces.model.DataModel;
+import javax.faces.model.ListDataModel;
 
 import org.open.erp.services.personal.Angajat;
 import org.open.erp.services.personal.PersonalSrvLocal;
 import org.open.erp.services.salarizare.Pontaj;
 import org.open.erp.services.salarizare.SalarizareSrvLocal;
-import org.open.erp.services.salarizare.Spor;
 import org.open.erp.services.salarizare.impl.SalarizareLogger;
 
 import com.google.common.base.Functions;
@@ -24,9 +25,13 @@ import com.google.common.collect.Ordering;
 
 public class FormPontaj implements Converter{
 
+	private Integer luna;
+	private Integer an;
 	private Pontaj pontaj;
 	private List<Pontaj> pontaje = new ArrayList<Pontaj>();
 	private List<Angajat> angajati = new ArrayList<Angajat>();
+	
+	private List<Integer> listAni = new ArrayList<Integer>();
 	
 	private Map<String, Integer> mapLuni = new HashMap<String, Integer>();
 	private Map<String, Integer> mapAni = new HashMap<String, Integer>();
@@ -47,6 +52,13 @@ public class FormPontaj implements Converter{
 		angajati = (List<Angajat>) personalSrv.getListaAngajati();
 		if (!pontaje.isEmpty())
 			pontaj = pontaje.get(0);
+		
+		listAni.add(2009);
+		listAni.add(2010);
+		listAni.add(2011);
+		listAni.add(2012);
+		listAni.add(2013);
+		//luna = mapLuni.get(0);
 	}
 
 	public Pontaj getPontaj() {
@@ -79,6 +91,14 @@ public class FormPontaj implements Converter{
 		this.mapLuni = mapLuni;
 	}
 	
+	public List<Integer> getListAni() {
+		return listAni;
+	}
+
+	public void setListAni(List<Integer> listAni) {
+		this.listAni = listAni;
+	}
+
 	public Map<String, Angajat> getAngajati(){
 		Map<String, Angajat> mapAngajati = new HashMap<String, Angajat>();
 		for (Angajat a: angajati){
@@ -94,13 +114,22 @@ public class FormPontaj implements Converter{
 	//operatie invocata la selectie din lista, dar inainte de setSpor
 	@Override
 	public Object getAsObject(FacesContext arg0, UIComponent uiComp, String uiValue) {
+		if (uiComp.getId().equals("cboLuna")){
+			logger.logINFO("<<<<<<< Luna din array este:"+uiValue);
+			luna = Integer.valueOf(uiValue);
+			return luna;
+		}
+		if (uiComp.getId().equals("cboAn")){
+			logger.logINFO("<<<<<<< Anul din array este:"+uiValue);
+			an = Integer.valueOf(uiValue);
+			return an;
+		}
 		if (uiComp.getId().equals("cboAngajat")){
 			Angajat uiAngajatTemplate = new Angajat();
 			uiAngajatTemplate.setId(Integer.valueOf(uiValue));
 			//logger.logINFO("Id-ul din array este:"+idx);
 			return this.angajati.get(this.angajati.indexOf(uiAngajatTemplate));
 		}
-		
 		return null;
 	}
 
@@ -109,12 +138,56 @@ public class FormPontaj implements Converter{
 	@Override
 	public String getAsString(FacesContext arg0, UIComponent uiComp, Object uiValue) {
 		if (uiComp.getId().equals("cboAngajat")){
-			//logger.logINFO("getAsString uiValue:"+uiValue.toString());
+			logger.logINFO("<<<<<<<<<< getAsString uiValue:"+uiValue.toString());
 			Angajat uiAngajat = (Angajat)uiValue;
 			//logger.logINFO("getAsString uiValue 2:"+uiSpor.getIdSpor());
 			if (uiAngajat.getId()!=null) //poate veni null din click Add
 				return uiAngajat.getId().toString();
 		}
+		if (uiComp.getId().equals("cboLuna")){
+			logger.logINFO("<<<<<<<< getAsString uiValue Luna:"+uiValue);
+			if (uiValue!=null)
+				return uiValue.toString();
+		}
+		if (uiComp.getId().equals("cboAn")){
+			logger.logINFO("<<<<<<<< getAsString uiValue An:"+uiValue);
+			if (uiValue!=null)
+				return uiValue.toString();
+		}
 		return null;
 	}
+	/*
+	public DataModel<Pontaj> getPontaje() {
+		logger.logINFO("Check model pontaje ... ");
+		if (this.pontaje == null)
+			populareModelPontaje();
+		return pontaje;
+	}
+	
+	private void populareModelPontaje(){
+		if (luna != null){
+			this.activitati = new ListDataModel<Activitate>();
+			this.activitati.setWrappedData(proiect.getActivitati());
+			logger.debug("Populare model activitati ... DEBUG ");
+		}else
+			this.activitati = null;
+	}
+*/
+	public Integer getLuna() {
+		return luna;
+	}
+
+	public void setLuna(Integer luna) {
+		this.luna = luna;
+	}
+
+	public Integer getAn() {
+		return an;
+	}
+
+	public void setAn(Integer an) {
+		this.an = an;
+	}
+	
+	
 }
