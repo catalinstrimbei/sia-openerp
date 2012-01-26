6b638dddeb45d162c170af9a06efa1052a72376a
cristina.macuc@gmail.com <cristina.macuc@gmail.com@01a5fee1-9287-8e58-6871-dad33e70e5c6>
2012-01-26 19:53:15 +0000

    Echipa Achizitii - logger

--- trunk/OpenERP/OpenERP_ACHIZITII/src/org/open/erp/services/achizitii/registri/RegistruAprovizionare.java
+++ trunk/OpenERP/OpenERP_ACHIZITII/src/org/open/erp/services/achizitii/registri/RegistruAprovizionare.java
@@ -3,9 +3,11 @@ package org.open.erp.services.achizitii.registri;
 import javax.persistence.EntityManager;
 
 
+
 public class RegistruAprovizionare {
 	
-		
+	private static org.apache.log4j.Logger logger = org.apache.log4j.Logger.getLogger(RegistruAprovizionare.class.getName());	
+
 		/* set up */
 		private EntityManager entityManager;
 		public RegistruAprovizionare(EntityManager em) {
--- trunk/OpenERP/OpenERP_ACHIZITII/src/org/open/erp/services/achizitii/registri/RegistruAprovizionareEJB.java
+++ trunk/OpenERP/OpenERP_ACHIZITII/src/org/open/erp/services/achizitii/registri/RegistruAprovizionareEJB.java
@@ -1,5 +1,14 @@
 package org.open.erp.services.achizitii.registri;
 
+import javax.persistence.EntityManager;
+
 public class RegistruAprovizionareEJB {
 
+	private static org.apache.log4j.Logger logger = org.apache.log4j.Logger.getLogger(RegistruAprovizionareEJB.class.getName());	
+
+	/* set up */
+	private EntityManager entityManager;
+	public RegistruAprovizionareEJB(EntityManager em) {
+		entityManager = em;
+	}
 }
