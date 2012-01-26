98405dfcf44f405a4a72b840cd62a861e7d041a5
cristina.macuc@gmail.com <cristina.macuc@gmail.com@01a5fee1-9287-8e58-6871-dad33e70e5c6>
2012-01-26 19:47:42 +0000

    Echipa Achizitii - RegistruAprovizionare

--- trunk/OpenERP/OpenERP_ACHIZITII/src/org/open/erp/services/achizitii/registri/RegistruAprovizionare.java
+++ trunk/OpenERP/OpenERP_ACHIZITII/src/org/open/erp/services/achizitii/registri/RegistruAprovizionare.java
@@ -1,5 +1,15 @@
 package org.open.erp.services.achizitii.registri;
 
+import javax.persistence.EntityManager;
+
+
 public class RegistruAprovizionare {
+	
+		
+		/* set up */
+		private EntityManager entityManager;
+		public RegistruAprovizionare(EntityManager em) {
+			entityManager = em;
+		}
 
 }
