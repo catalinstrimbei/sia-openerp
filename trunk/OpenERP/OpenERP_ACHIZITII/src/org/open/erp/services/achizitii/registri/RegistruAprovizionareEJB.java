6d7ecd64ce2c51799e224eb41a65ed22aa60dd88
bogdandcn@gmail.com <bogdandcn@gmail.com@01a5fee1-9287-8e58-6871-dad33e70e5c6>
2012-01-26 22:30:43 +0000

    OPEN_ERP_ACHIZITII-noi-registri

--- trunk/OpenERP/OpenERP_ACHIZITII/src/org/open/erp/services/achizitii/registri/RegistruAprovizionareEJB.java
+++ trunk/OpenERP/OpenERP_ACHIZITII/src/org/open/erp/services/achizitii/registri/RegistruAprovizionareEJB.java
@@ -4,6 +4,10 @@ import java.util.Collection;
 
 import javax.persistence.EntityManager;
 
+import org.open.erp.services.achizitii.Articol;
+import org.open.erp.services.achizitii.Categorie;
+import org.open.erp.services.achizitii.CerereOferta;
+import org.open.erp.services.achizitii.Comanda;
 import org.open.erp.services.achizitii.Furnizor;
 import org.open.erp.services.achizitii.OfertaAchizitie;
 
@@ -25,6 +29,61 @@ public class RegistruAprovizionareEJB {
 											.getResultList();
 		}catch(Exception ex){
 			logger.error("ERROR: "+ex.getMessage());
+			throw ex;
+		}
+	}
+	
+	public Collection<Articol> getArticolePeCategorii(Categorie categorie_) throws Exception{
+		try{
+			return entityManager.createQuery("SELECT a FROM Articol a where a.categorieArticol = :categorie")
+											.setParameter("categorie", categorie_)
+											.getResultList();
+		}catch(Exception ex){
+			logger.error("ERROR "  + ex.getMessage());
+			throw ex;
+		}
+	}
+	
+	public Collection<Categorie> getCategoriiPeFurnizori(Furnizor furnizor_) throws Exception{
+		try{
+			return entityManager.createQuery("SELECT c FROM Categorie c where c.furnizoriCategorie = :furnizor")
+											.setParameter("furnizor", furnizor_)
+											.getResultList();
+		}catch(Exception ex){
+			logger.error("ERROR "  + ex.getMessage());
+			throw ex;
+		}
+	}
+	
+	public Collection<CerereOferta> getCereriOfertaPeStatus(Integer statusCerereOferta_) throws Exception{
+		try{
+			return entityManager.createQuery("SELECT co FROM CerereOferta co where co.statusCerereOferta = :status")
+											.setParameter("status", statusCerereOferta_)
+											.getResultList();
+		}catch(Exception ex){
+			logger.error("ERROR "  + ex.getMessage());
+			throw ex;
+		}
+	}
+	
+	public Collection<Comanda> getcomenziPeFurnizor(Furnizor furnizor_) throws Exception{
+		try{
+			return entityManager.createQuery("SELECT c FROM Comanda c where c.furnizor = :furnizor")
+											.setParameter("furnizor", furnizor_)
+											.getResultList();
+		}catch(Exception ex){
+			logger.error("ERROR "  + ex.getMessage());
+			throw ex;
+		}
+	}
+	
+	public Collection<Comanda> getComenziPeStatus(Integer statusComanda_) throws Exception{
+		try{
+			return entityManager.createQuery("SELECT c FROM Comanda c where c.statusComanda = :status")
+											.setParameter("status", statusComanda_)
+											.getResultList();
+		}catch(Exception ex){
+			logger.error("ERROR "  + ex.getMessage());
 			throw ex;
 		}
 	}
