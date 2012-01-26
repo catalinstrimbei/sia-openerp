71e653a48be7cbc9209caed0ffec3fb64da2131f
Constantin.popa10@gmail.com <Constantin.popa10@gmail.com@01a5fee1-9287-8e58-6871-dad33e70e5c6>
2012-01-26 22:21:43 +0000

    Team Achizitii - Adaugare metoda registri

--- trunk/OpenERP/OpenERP_ACHIZITII/src/org/open/erp/services/achizitii/registri/RegistruAprovizionareEJB.java
+++ trunk/OpenERP/OpenERP_ACHIZITII/src/org/open/erp/services/achizitii/registri/RegistruAprovizionareEJB.java
@@ -1,6 +1,11 @@
 package org.open.erp.services.achizitii.registri;
 
+import java.util.Collection;
+
 import javax.persistence.EntityManager;
+
+import org.open.erp.services.achizitii.Furnizor;
+import org.open.erp.services.achizitii.OfertaAchizitie;
 
 public class RegistruAprovizionareEJB {
 
@@ -11,4 +16,17 @@ public class RegistruAprovizionareEJB {
 	public RegistruAprovizionareEJB(EntityManager em) {
 		entityManager = em;
 	}
+	@SuppressWarnings("unchecked")
+	public Collection<OfertaAchizitie> getOferteAchizitiePerFurnizor(Furnizor furnizor_) throws Exception{
+		try{
+			return entityManager.createQuery("SELECT oa FROM OfertaAchizitie oa " +
+											"WHERE oa.furnizor = :furnizor_")
+											.setParameter("furnizor_", furnizor_)
+											.getResultList();
+		}catch(Exception ex){
+			logger.error("ERROR: "+ex.getMessage());
+			throw ex;
+		}
+	}
 }
+
