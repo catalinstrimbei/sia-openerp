437152698a6cf05ff2cf84d78c59a13970f33e0c
ionut.hrubaru@gmail.com <ionut.hrubaru@gmail.com@01a5fee1-9287-8e58-6871-dad33e70e5c6>
2012-01-26 22:01:39 +0000

    SALARIZARE -Ionut Hrubaru - adaugat suport pt validari

---
+++ trunk/OpenERP/OpenERP_SALARIZARE_JSF/src/org/open/erp/ui/RequiredValidator.java
@@ -0,0 +1,37 @@
+package org.open.erp.ui;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.validator.Validator;
+import javax.faces.validator.ValidatorException;
+
+public class RequiredValidator implements Validator {
+
+	  public void validate(FacesContext context, UIComponent component, Object value) throws ValidatorException {
+
+	    if (value == null || "".equals(value.toString().trim())) {
+
+	      FacesMessage message = new FacesMessage();
+
+	      String messageStr = (String)component.getAttributes().get("message");
+
+	      if (messageStr == null) {
+
+	        messageStr = "Please enter data";
+
+	      }
+
+	      message.setDetail(messageStr);
+
+	      message.setSummary(messageStr);
+
+	      message.setSeverity(FacesMessage.SEVERITY_ERROR);
+
+	      throw new ValidatorException(message);
+
+	    }
+
+	  }
+
+	}
