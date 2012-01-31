bda32659f03bb7c1a255264fb021aa54e705b1a5
adandronic@gmail.com <adandronic@gmail.com@01a5fee1-9287-8e58-6871-dad33e70e5c6>
2012-01-31 10:39:41 +0000

    CONTABGEST - Andreea Andronic - Modificare registru si impl

--- trunk/OpenERP/OpenERP_CONTABGEST/src/org/open/erp/services/contabgest/impl/ContabGestImpl.java
+++ trunk/OpenERP/OpenERP_CONTABGEST/src/org/open/erp/services/contabgest/impl/ContabGestImpl.java
@@ -1,8 +1,11 @@
 package org.open.erp.services.contabgest.impl;
 
+import java.util.Date;
+
 import javax.annotation.PostConstruct;
 import javax.annotation.Resource;
 import javax.ejb.SessionContext;
+
 import javax.ejb.Stateless;
 import javax.ejb.TransactionAttribute;
 import javax.ejb.TransactionAttributeType;
@@ -11,23 +14,32 @@ import javax.ejb.TransactionManagementType;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
+import org.open.erp.services.contabgest.CentruCost;
 import org.open.erp.services.contabgest.CheltuieliFixe;
+import org.open.erp.services.contabgest.CheltuieliVariabile;
+import org.open.erp.services.contabgest.ContabGestSrv;
 import org.open.erp.services.contabgest.ContabGestSrvLocal;
 import org.open.erp.services.contabgest.ContabGestSrvRemote;
+import org.open.erp.services.contabgest.DummyFazaProductie;
+import org.open.erp.services.contabgest.DummyPersoana;
+import org.open.erp.services.contabgest.ProceseTehnicoEconomice;
 import org.open.erp.services.contabgest.ProdusFinit;
+import org.open.erp.services.contabgest.ResponabilCentruCost;
+import org.open.erp.services.contabgest.TipCheltuieli;
 import org.open.erp.services.contabgest.exceptions.ContabGestLogger;
+
 
 
 @Stateless(name="ContabGestSrv")
 @TransactionManagement(TransactionManagementType.CONTAINER)
-public class ContabGestImpl implements  ContabGestSrvLocal, ContabGestSrvRemote {
+public class ContabGestImpl implements ContabGestSrv, ContabGestSrvLocal, ContabGestSrvRemote {
 
 	private static org.apache.log4j.Logger logger = org.apache.log4j.Logger.getLogger(ContabGestImpl.class.getName());
 	ContabGestLogger logger2 = new ContabGestLogger();
 	
 	private RegistruContabGest registruContabGest;
 	
-	//private static ContabGestSrv contabGest;
+	private static ContabGestSrv contabGest;
 	
 	@PersistenceContext(unitName="OpenERP_CONTABGEST")
 	private EntityManager em;
@@ -52,7 +64,7 @@ public class ContabGestImpl implements  ContabGestSrvLocal, ContabGestSrvRemote 
 	
 	
 	
-	/*
+	/* implementare actiuni serviciu ProjectManagementSrv */
 	@TransactionAttribute(TransactionAttributeType.REQUIRED)
 	@Override
 	public DummyPersoana defDummyPersoana( String nume,
@@ -63,7 +75,7 @@ public class ContabGestImpl implements  ContabGestSrvLocal, ContabGestSrvRemote 
 		logger.debug(">>>>>>>>>>>> START Creare persoana");		
 		DummyPersoana dummyPersoanaNou = new DummyPersoana(nume, prenume, 
 											formaAdresare);
-		
+		/* Actiune tranzactionala ... */
 		if (sessionContext.getRollbackOnly() == true){
 			logger.debug(">>>>>>>>>>>> END Creare persoana - TRANZACTIE ANULATA");
 			//throw new RuntimeException("Creare proiect - TRANZACTIE ANULATA");
@@ -71,6 +83,7 @@ public class ContabGestImpl implements  ContabGestSrvLocal, ContabGestSrvRemote 
 			dummyPersoanaNou = this.registruContabGest.salveazaPersoana(dummyPersoanaNou);
 			//em.persist(proiectNou);
 		}	
+		System.out.println("!>>>>>>>>>>>> END Creare persoana");
 		logger.debug(">>>>>>>>>>>> END Creare persoana");
 		return dummyPersoanaNou;
 	}
@@ -91,7 +104,6 @@ public class ContabGestImpl implements  ContabGestSrvLocal, ContabGestSrvRemote 
 		}
 		return result;
 	}
-	*/
 	
 	@TransactionAttribute(TransactionAttributeType.REQUIRED)
 	@Override
@@ -107,11 +119,12 @@ public class ContabGestImpl implements  ContabGestSrvLocal, ContabGestSrvRemote 
 			produFinitNou = this.registruContabGest.salveazaProdusFinit(produFinitNou);
 			//em.persist(proiectNou);
 		}	
+		System.out.println("!>>>>>>>>>>>> END Creare produs finit");
 		logger.debug(">>>>>>>>>>>> END Creare produs finit");
 		return produFinitNou;
 	}
 	
-
+	
 	@TransactionAttribute(TransactionAttributeType.REQUIRED)
 	@Override
 	public CheltuieliFixe defCheltuieliFixe(String tipCheltuiala, String denCheltuiala,
@@ -133,21 +146,230 @@ public class ContabGestImpl implements  ContabGestSrvLocal, ContabGestSrvRemote 
 		System.out.println("!>>>>>>>>>>>> END Creare cheltuiala ");
 		return cheltuieliFixeNou;
 	}
-  @Override
-	public ProdusFinit getProdusFinit(Integer idProdusFinit)
-			throws Exception {
+
+
+	@TransactionAttribute(TransactionAttributeType.REQUIRED)
+	@Override
+	public CheltuieliVariabile defCheltuieliVariabile(String tipCheltuiala, String denCheltuialaV,
+			String delatiiCheltuialaV, Integer cantitateCheltuialaV) throws Exception{
+		System.out.println("!>>>>>>>>>>>> START Creare cheltuiala ");
+		logger.debug(">>>>>>>>>>>> START tipcheltuiala");
+		//TipCheltuieli tipCheltuialaNou = new TipCheltuieli(tipCheltuiala);
+		CheltuieliVariabile cheltuieliVariabileNou = new CheltuieliVariabile(denCheltuialaV, delatiiCheltuialaV, cantitateCheltuialaV);
+		/* Actiune tranzactionala ... */
+		if (sessionContext.getRollbackOnly() == true){
+			logger.debug(">>>>>>>>>>>> END Creare cheltuiala fixa - TRANZACTIE ANULATA");
+			//throw new RuntimeException("Creare proiect - TRANZACTIE ANULATA");
+		}else{
+			//tipCheltuialaNou= this.registruContabGest.salveazaTipCheltuiala(tipCheltuialaNou);
+			cheltuieliVariabileNou = this.registruContabGest.salveazaCheltuieliVariabile(cheltuieliVariabileNou);
+			//em.persist(proiectNou);
+		}	
+		logger.debug(">>>>>>>>>>>> END Creare cheltuiala fixa");
+		System.out.println("!>>>>>>>>>>>> END Creare cheltuiala ");
+		return cheltuieliVariabileNou;
+	}
+	
+	@TransactionAttribute(TransactionAttributeType.REQUIRED)
+	@Override
+	public ProceseTehnicoEconomice defProceseTehnicoEconomice(String	denumireProces,
+			String detaliiProces) throws Exception{
+		System.out.println("!>>>>>>>>>>>> START Creare tip proces tehnico-economic ");
+		logger.debug(">>>>>>>>>>>> START proces tehnico-economic");
+		
+		ProceseTehnicoEconomice proceseTehnicoEconomiceNou = new ProceseTehnicoEconomice(denumireProces, detaliiProces);
+		/* Actiune tranzactionala ... */
+		if (sessionContext.getRollbackOnly() == true){
+			logger.debug(">>>>>>>>>>>> END Creare proces tehnico-economic - TRANZACTIE ANULATA");
+		}else{
+			//tipCheltuialaNou= this.registruContabGest.salveazaTipCheltuiala(tipCheltuialaNou);
+			proceseTehnicoEconomiceNou = this.registruContabGest.salveazaTipProceseTehnicoEconomice(proceseTehnicoEconomiceNou);
+			//em.persist(proiectNou);
+		}	
+		logger.debug(">>>>>>>>>>>> END Creare proces tehnico-economic");
+		System.out.println("!>>>>>>>>>>>> END Creare proces tehnico-economic ");
+		return proceseTehnicoEconomiceNou;
+		
+	}
+	
+
+	@TransactionAttribute(TransactionAttributeType.REQUIRED)
+	@Override
+	public ResponabilCentruCost defResponabilCentruCost(String  nume, String  prenume, String  formaAdresare,
+			Integer idResponsabilCentruCost, Date dataStartResponsabilitate, 
+			Date dataSfarsitResponsabilitate, String detaliiResponsabilitati) throws Exception {
+		
+		System.out.println("!>>>>>>>>>>>> START Creare Responsabil centru cost ");
+		logger.debug(">>>>>>>>>>>> START Responsabil centru cost");
+		
+		ResponabilCentruCost responabilCentruCostNou = new ResponabilCentruCost(nume, prenume, formaAdresare,
+				idResponsabilCentruCost, dataStartResponsabilitate, dataSfarsitResponsabilitate, detaliiResponsabilitati);
+
+		if (sessionContext.getRollbackOnly() == true){
+			logger.debug(">>>>>>>>>>>> END Creare Responsabil centru cost - TRANZACTIE ANULATA");
+		}else{
+			
+			responabilCentruCostNou = this.registruContabGest.salveazaResponabilCentruCost(responabilCentruCostNou);
+			
+		}	
+		
+		logger.debug(">>>>>>>>>>>> END Creare Responsabil centru cost");
+		System.out.println("!>>>>>>>>>>>> END Creare Responsabil centru cost");
+		return responabilCentruCostNou;
+		
+		
+	}
+	
+	
+	
+	@TransactionAttribute(TransactionAttributeType.REQUIRED)
+	@Override
+	public DummyFazaProductie defDummyFazaProductie(String denumireFazaProductie,
+			Date incepereFazaProductie, Date sfarsitFazaProductie, Double costFazaProductie) throws Exception{
+		System.out.println("!>>>>>>>>>>>> START Creare DummyFazaProductie");
+		logger.debug(">>>>>>>>>>>> START DummyFazaProductie");		
+		DummyFazaProductie dummyFazaProductieNoua = new DummyFazaProductie(denumireFazaProductie, incepereFazaProductie,
+				sfarsitFazaProductie, costFazaProductie);
+		if (sessionContext.getRollbackOnly() == true){
+			logger.debug(">>>>>>>>>>>> END Creare DummyFazaProductie - TRANZACTIE ANULATA");
+			
+		}else{
+			dummyFazaProductieNoua = this.registruContabGest.salveazaDummyFazaProductie(dummyFazaProductieNoua);
+		}	
+		System.out.println("!>>>>>>>>>>>> END Creare dummyFazaProductie");
+		logger.debug(">>>>>>>>>>>> END Creare dummyFazaProductie");
+		return dummyFazaProductieNoua;
+	}
+
+
+	
+	
+		
+	
+	
+	@TransactionAttribute(TransactionAttributeType.REQUIRED)
+	@Override
+	public CentruCost defCentruCost(String denCentruCost, Date startCentruCost, Date sfarsitCentruCost, 
+			ResponabilCentruCost responabilCentruCost, DummyFazaProductie dummyFazaProductie,
+			ProceseTehnicoEconomice proceseTehnicoEconomice, ProdusFinit produsfinit) throws Exception{
+		System.out.println("!>>>>>>>>>>>> START Creare centru cost ");
+		logger.debug(">>>>>>>>>>>> START centru cost");
+		
+		CentruCost centruCostNou = new CentruCost(denCentruCost, startCentruCost, sfarsitCentruCost, 
+				responabilCentruCost, dummyFazaProductie, proceseTehnicoEconomice,  produsfinit);
+
+		if (sessionContext.getRollbackOnly() == true){
+			logger.debug(">>>>>>>>>>>> END Creare centru cost - TRANZACTIE ANULATA");
+		}else{
+			
+			centruCostNou = this.registruContabGest.salveazaCentruCost(centruCostNou);
+			
+		}	
+		logger.debug(">>>>>>>>>>>> END Creare centru cost");
+		System.out.println("!>>>>>>>>>>>> END Creare centru cost ");
+		return centruCostNou;
+		
+	}
+	
+	@TransactionAttribute(TransactionAttributeType.REQUIRED)
+	@Override
+	public ProdusFinit getProdusFinitById(Integer idProdusFinit_) throws Exception{
 		logger.debug(" Start >> " + Thread.currentThread().getStackTrace()[1].getMethodName());
 		ProdusFinit result = new ProdusFinit();
-		if (idProdusFinit == null){				
+		if (idProdusFinit_ == null){					
 			sessionContext.setRollbackOnly();
 			logger.debug(" Transaction Canceled in >> " + Thread.currentThread().getStackTrace()[1].getMethodName());
 		}
 		else{			
-			result = this.registruContabGest.getProdusFinitIdR(idProdusFinit); 
+			result = this.registruContabGest.getProdusFinitById(idProdusFinit_);
 			
-			logger.debug(">>>>>>End getPersonaID");
+			logger.debug(">>>>>>End getProdusFinitById");
 		}
 		return result;
 	}
+	
+	@TransactionAttribute(TransactionAttributeType.REQUIRED)
+	@Override
+	public DummyFazaProductie getDummyFazaProductieById(Integer idFazaProductie_) throws Exception{
+		logger.debug(" Start >> " + Thread.currentThread().getStackTrace()[1].getMethodName());
+		DummyFazaProductie result = new DummyFazaProductie();
+		if (idFazaProductie_ == null){					
+			sessionContext.setRollbackOnly();
+			logger.debug(" Transaction Canceled in >> " + Thread.currentThread().getStackTrace()[1].getMethodName());
+		}
+		else{			
+			result = this.registruContabGest.getDummyFazaProductieById(idFazaProductie_);
+			
+			logger.debug(">>>>>>End getDummyFazaProductieById");
+		}
+		return result;
+	}
+	
+	
+	@TransactionAttribute(TransactionAttributeType.REQUIRED)
+	@Override
+	public ResponabilCentruCost getResponabilCentruCostById(Integer idResponsabilCentruCost_) throws Exception{
+		logger.debug(" Start >> " + Thread.currentThread().getStackTrace()[1].getMethodName());
+		ResponabilCentruCost result = new ResponabilCentruCost();
+		if (idResponsabilCentruCost_ == null){					
+			sessionContext.setRollbackOnly();
+			logger.debug(" Transaction Canceled in >> " + Thread.currentThread().getStackTrace()[1].getMethodName());
+		}
+		else{			
+			result = this.registruContabGest.getResponabilCentruCostById(idResponsabilCentruCost_);
+			
+			logger.debug(">>>>>>End getResponabilCentruCostById");
+		}
+		return result;
+	}
+	
+	
+	@TransactionAttribute(TransactionAttributeType.REQUIRED)
+	@Override
+	public ProceseTehnicoEconomice getProceseTehnicoEconomiceById(Integer idProceseTehnicoEconomice_) throws Exception{
+		logger.debug(" Start >> " + Thread.currentThread().getStackTrace()[1].getMethodName());
+		ProceseTehnicoEconomice result = new ProceseTehnicoEconomice();
+		if (idProceseTehnicoEconomice_ == null){					
+			sessionContext.setRollbackOnly();
+			logger.debug(" Transaction Canceled in >> " + Thread.currentThread().getStackTrace()[1].getMethodName());
+		}
+		else{			
+			result = this.registruContabGest.getProceseTehnicoEconomiceById(idProceseTehnicoEconomice_);
+			
+			logger.debug(">>>>>>End getProceseTehnicoEconomiceById");
+		}
+		return result;
+	}
+
+
+	
+	
+	/*
+	@TransactionAttribute(TransactionAttributeType.REQUIRED)
+	@Override
+	public ResponabilCentruCost getIdResponsabilCentruCostdupaId(int idResponsabilCentruCost){
+		
+		System.out.println("!>>>>>>>>>>>> START Gasire Responsabil ");
+		logger.debug(">>>>>>>>>>>> START START Gasire Responsabil");
+		
+		//CentruCost centruCostNou = new CentruCost(denCentruCost, startCentruCost, sfarsitCentruCost, 
+		//		responabilCentruCost, dummyFazaProductie, proceseTehnicoEconomice,  produsfinit);
+
+		if (sessionContext.getRollbackOnly() == true){
+			logger.debug(">>>>>>>>>>>> END Creare centru cost - TRANZACTIE ANULATA");
+		}else{
+			
+		//	centruCostNou = this.registruContabGest.salveazaCentruCost(centruCostNou);
+			
+		}	
+		logger.debug(">>>>>>>>>>>> END Creare centru cost");
+		System.out.println("!>>>>>>>>>>>> END Creare centru cost ");
+		return null;
+	}
+	*/
+	
+	
+	
+	
 
 }
--- trunk/OpenERP/OpenERP_CONTABGEST/src/org/open/erp/services/contabgest/impl/RegistruContabGest.java
+++ trunk/OpenERP/OpenERP_CONTABGEST/src/org/open/erp/services/contabgest/impl/RegistruContabGest.java
@@ -7,10 +7,18 @@ import java.util.List;
 import javax.persistence.EntityManager;
 
 import org.apache.log4j.Logger;
-//import org.open.erp.services.contabgest.DummyPersoana;
+import org.open.erp.services.contabgest.CentruCost;
 import org.open.erp.services.contabgest.CheltuieliFixe;
+import org.open.erp.services.contabgest.CheltuieliVariabile;
+import org.open.erp.services.contabgest.DummyFazaProductie;
+import org.open.erp.services.contabgest.DummyPersoana;
+import org.open.erp.services.contabgest.ProceseTehnicoEconomice;
 import org.open.erp.services.contabgest.ProdusFinit;
+import org.open.erp.services.contabgest.ResponabilCentruCost;
+import org.open.erp.services.contabgest.TipCheltuieli;
 import org.open.erp.services.contabgest.exceptions.ContabGestLogger;
+
+
 
 
 public class RegistruContabGest {
@@ -22,6 +30,33 @@ public class RegistruContabGest {
 	private EntityManager entityManager;
 	public RegistruContabGest(EntityManager em) {
 		entityManager = em;
+	}
+	
+	/* interogari */
+	public DummyPersoana getPPers(Integer id){
+		return entityManager.find(DummyPersoana.class, id);
+	}
+	
+	public List<DummyPersoana> getToatePersoanele(){
+		return entityManager.createQuery("SELECT p FROM DummyPersoana p").getResultList();
+	}
+	
+	/* persistenta */
+	public DummyPersoana salveazaPersoana(DummyPersoana dummyPersoana) throws Exception{
+		try{
+			
+			if (dummyPersoana.getId() == null || 
+				entityManager.find(dummyPersoana.getClass(), dummyPersoana.getId()) == null)
+				entityManager.persist(dummyPersoana);
+			else
+				entityManager.merge(dummyPersoana);
+			
+		}catch(Exception ex){
+			logger.info("EROARE PERSISTENTA ***** ");
+			ex.printStackTrace();
+			throw ex;
+		}
+		return dummyPersoana;
 	}
 	
 	public ProdusFinit salveazaProdusFinit(ProdusFinit produsFinit) throws Exception{
@@ -39,21 +74,6 @@ public class RegistruContabGest {
 		}
 		return produsFinit;
 	}
-
-	public ProdusFinit getProdusFinitIdR(Integer idProdusFinit) throws Exception
-	{
-		try{
-			return entityManager.find(ProdusFinit.class, idProdusFinit);
-		}
-		catch(Exception ex)
-		{
-			logger2.logERROR("Persistence Error in method >> "  + Thread.currentThread().getStackTrace()[1].getMethodName());
-			logger2.logERROR("Class >> " + ex.getClass().toString() + "<< StackTrace >> " + ex.getStackTrace().toString() + "<< Error >> " + ex.getMessage().toString());
-			ex.printStackTrace();   StringWriter st = new StringWriter(); PrintWriter pt = new PrintWriter(st); ex.printStackTrace(pt); logger2.logERROR("<< Stack Trace >>" + st.toString());
-			throw ex;
-		}
-	}	
-	
 	
 	public CheltuieliFixe salveazaCheltuieliFixe(CheltuieliFixe cheltuieliFixe) throws Exception{
 		try{
@@ -71,62 +91,26 @@ public class RegistruContabGest {
 		return cheltuieliFixe;
 	}
 	
-	/* persistenta */
-	public ProdusFinit salveazaProdus(ProdusFinit produsFinit) throws Exception{
+	
+	
+	public CheltuieliVariabile salveazaCheltuieliVariabile(CheltuieliVariabile cheltuieliVariabile) throws Exception{
 		try{
-			
-			//if (!entityManager.contains(proiect)) /* o posibilitate de verificare */
-			if (produsFinit.getIdProdusFinit() == null || /* proiect.getIdProiect() pentru proiect cu id generat*/
-				entityManager.find(produsFinit.getClass(), produsFinit.getIdProdusFinit()) == null)
-				entityManager.persist(produsFinit);
+			if (cheltuieliVariabile.getIdTipCheltuieli() == null || 
+				entityManager.find(cheltuieliVariabile.getClass(), cheltuieliVariabile.getIdTipCheltuieli()) == null)
+				entityManager.persist(cheltuieliVariabile);
 			else
-				entityManager.merge(produsFinit);
+				entityManager.merge(cheltuieliVariabile);
 			
 		}catch(Exception ex){
 			logger.info("EROARE PERSISTENTA ***** ");
 			ex.printStackTrace();
 			throw ex;
 		}
-		return produsFinit;
+		return cheltuieliVariabile;
 	}
 	
-	public List<ProdusFinit> getToateProdusele(){
-		return entityManager.createQuery("SELECT p FROM ProdusFinit p").getResultList();
-	}
 	
-	/* interogari */
-	/*
-	public DummyPersoana getPPers(Integer id){
-		return entityManager.find(DummyPersoana.class, id);
-	}
 	
-	public List<DummyPersoana> getToatePersoanele(){
-		return entityManager.createQuery("SELECT p FROM DummyPersoana p").getResultList();
-	}
-	*/
-	
-	/* persistenta */
-	/*
-	public DummyPersoana salveazaPersoana(DummyPersoana dummyPersoana) throws Exception{
-		try{
-			
-			if (dummyPersoana.getId() == null || 
-				entityManager.find(dummyPersoana.getClass(), dummyPersoana.getId()) == null)
-				entityManager.persist(dummyPersoana);
-			else
-				entityManager.merge(dummyPersoana);
-			
-		}catch(Exception ex){
-			logger.info("EROARE PERSISTENTA ***** ");
-			ex.printStackTrace();
-			throw ex;
-		}
-		return dummyPersoana;
-	}
-	*/
-
-	
-	/*
 	public DummyPersoana getPersoanaIdR(Integer idPersoana) throws Exception
 	{
 		try{
@@ -144,12 +128,199 @@ public class RegistruContabGest {
 	
 	
 
-	public void stergeProiect(DummyPersoana dummyPersoana){
+	public void stergePersoana(DummyPersoana dummyPersoana){
 		entityManager.remove(dummyPersoana);
 	}
 	
 	public void refreshPersoana(DummyPersoana dummyPersoana){
 		entityManager.refresh(dummyPersoana);
 	}
-*/
+
+	public TipCheltuieli salveazaTipCheltuiala(TipCheltuieli tipCheltuieli) throws Exception {
+		try{
+			if (tipCheltuieli.getIdTipCheltuieli() == null || 
+				entityManager.find(tipCheltuieli.getClass(), tipCheltuieli.getIdTipCheltuieli()) == null)
+				entityManager.persist(tipCheltuieli);
+			else
+				entityManager.merge(tipCheltuieli);
+			
+		}catch(Exception ex){
+			
+			logger.info("EROARE PERSISTENTA ***** ");
+			ex.printStackTrace();
+			throw ex;
+		}
+		return tipCheltuieli;
+	}
+	
+	
+	public ProceseTehnicoEconomice salveazaTipProceseTehnicoEconomice(ProceseTehnicoEconomice proceseTehnicoEconomice) throws Exception {
+		try{
+			if (proceseTehnicoEconomice.getIdProces() == null || 
+				entityManager.find(proceseTehnicoEconomice.getClass(), proceseTehnicoEconomice.getIdProces()) == null)
+				entityManager.persist(proceseTehnicoEconomice);
+			else
+				entityManager.merge(proceseTehnicoEconomice);
+			
+		}catch(Exception ex){
+			
+			logger.info("EROARE PERSISTENTA ***** ");
+			ex.printStackTrace();
+			throw ex;
+		}
+		return proceseTehnicoEconomice;
+	}
+	
+	
+
+	public CentruCost salveazaCentruCost(CentruCost centruCost) throws Exception {
+		try{
+			if (centruCost.getIdCentruCost() == null || 
+				entityManager.find(centruCost.getClass(), centruCost.getIdCentruCost()) == null)
+				entityManager.persist(centruCost);
+			else
+				entityManager.merge(centruCost);
+			
+		}catch(Exception ex){
+			
+			logger.info("EROARE PERSISTENTA ***** ");
+			ex.printStackTrace();
+			throw ex;
+		}
+		return centruCost;
+	}
+	
+
+	
+	
+	public DummyPersoana salveazaDummyPersoana(DummyPersoana dummyPersoana) throws Exception {
+		try{
+			if (dummyPersoana.getId() == null || 
+				entityManager.find(dummyPersoana.getClass(), dummyPersoana.getId()) == null)
+				entityManager.persist(dummyPersoana);
+			else
+				entityManager.merge(dummyPersoana);
+			
+		}catch(Exception ex){
+			
+			logger.info("EROARE PERSISTENTA ***** ");
+			ex.printStackTrace();
+			throw ex;
+		}
+		return dummyPersoana;
+	}
+	
+	public ProdusFinit getProdusFinitById(Integer idProdusFinit) throws Exception{
+		try{
+			//return entityManager.find(ProdusFinit.class, id);
+			//return (ProdusFinit) entityManager.createQuery("SELECT c FROM ProdusFinit c WHERE c.idProdusFinit = :id")
+			//		.setParameter("idProdusFinit", id)
+			//		.getSingleResult();
+			
+			return (ProdusFinit) entityManager.createQuery("SELECT c FROM ProdusFinit c WHERE c.idProdusFinit =:idProdusFinit").setParameter("idProdusFinit", idProdusFinit).getSingleResult();
+		}catch(Exception ex){
+			logger.error("Persistence Error in method >> "  + Thread.currentThread().getStackTrace()[1].getMethodName());
+			logger.error("Class >> " + ex.getClass().toString() + "<< StackTrace >> " + ex.getStackTrace().toString() + "<< Error >> " + ex.getMessage().toString());
+			ex.printStackTrace();   StringWriter st = new StringWriter(); PrintWriter pt = new PrintWriter(st); ex.printStackTrace(pt); logger.error("<< Stack Trace >>" + st.toString());
+			throw ex;
+		}
+	}
+	
+	
+	public DummyFazaProductie getDummyFazaProductieById(Integer idFazaProductie) throws Exception{
+		try{
+			//return entityManager.find(DummyFazaProductie.class, id);
+			//return  (DummyFazaProductie) entityManager.createQuery("SELECT d FROM DummyFazaProductie d WHERE d.idFazaProductie = :idFazaProductie")
+				//	.setParameter("idFazaProductie", idFazaProductie).getSingleResult();
+			
+			return (DummyFazaProductie) entityManager.createQuery("SELECT c FROM DummyFazaProductie c WHERE c.idFazaProductie =:idFazaProductie").setParameter("idFazaProductie", idFazaProductie).getSingleResult();
+			
+		}catch(Exception ex){
+			logger.error("Persistence Error in method >> "  + Thread.currentThread().getStackTrace()[1].getMethodName());
+			logger.error("Class >> " + ex.getClass().toString() + "<< StackTrace >> " + ex.getStackTrace().toString() + "<< Error >> " + ex.getMessage().toString());
+			ex.printStackTrace();   StringWriter st = new StringWriter(); PrintWriter pt = new PrintWriter(st); ex.printStackTrace(pt); logger.error("<< Stack Trace >>" + st.toString());
+			throw ex;
+		}
+	}
+	
+	public ResponabilCentruCost getResponabilCentruCostById(Integer id) throws Exception{
+		try{
+			//return entityManager.find(ResponabilCentruCost.class, id);
+			return (ResponabilCentruCost) entityManager.createQuery("SELECT c FROM ResponabilCentruCost c WHERE c.id = :id")
+					.setParameter("id", id)
+					.getSingleResult();
+		}catch(Exception ex){
+			logger.error("Persistence Error in method >> "  + Thread.currentThread().getStackTrace()[1].getMethodName());
+			logger.error("Class >> " + ex.getClass().toString() + "<< StackTrace >> " + ex.getStackTrace().toString() + "<< Error >> " + ex.getMessage().toString());
+			ex.printStackTrace();   StringWriter st = new StringWriter(); PrintWriter pt = new PrintWriter(st); ex.printStackTrace(pt); logger.error("<< Stack Trace >>" + st.toString());
+			throw ex;
+		}
+	}
+	
+	public ProceseTehnicoEconomice getProceseTehnicoEconomiceById(Integer idProces) throws Exception{
+		try{
+			//return entityManager.find(ProceseTehnicoEconomice.class, id);
+			//return (ProceseTehnicoEconomice) entityManager.createQuery("SELECT c FROM ProceseTehnicoEconomice c WHERE c.idProces= :id")
+			//		.setParameter("idProces", id)
+			//		.getSingleResult();
+			
+			return (ProceseTehnicoEconomice) entityManager.createQuery("SELECT c FROM ProceseTehnicoEconomice c WHERE c.idProces =:idProces").setParameter("idProces", idProces).getSingleResult();
+			
+		}catch(Exception ex){
+			logger.error("Persistence Error in method >> "  + Thread.currentThread().getStackTrace()[1].getMethodName());
+			logger.error("Class >> " + ex.getClass().toString() + "<< StackTrace >> " + ex.getStackTrace().toString() + "<< Error >> " + ex.getMessage().toString());
+			ex.printStackTrace();   StringWriter st = new StringWriter(); PrintWriter pt = new PrintWriter(st); ex.printStackTrace(pt); logger.error("<< Stack Trace >>" + st.toString());
+			throw ex;
+		}
+	}
+	
+	
+	
+	
+	
+	
+
+	
+	public ResponabilCentruCost salveazaResponabilCentruCost(ResponabilCentruCost responabilCentruCost) throws Exception {
+		try{
+			if (responabilCentruCost.getId() == null || 
+				entityManager.find(responabilCentruCost.getClass(), responabilCentruCost.getId()) == null)
+				entityManager.persist(responabilCentruCost);
+			else
+				entityManager.merge(responabilCentruCost);
+			
+		}catch(Exception ex){
+			
+			logger.info("EROARE PERSISTENTA ***** ");
+			ex.printStackTrace();
+			throw ex;
+		}
+		return responabilCentruCost;
+		
+	}
+
+	
+
+	public DummyFazaProductie salveazaDummyFazaProductie(
+			DummyFazaProductie dummyFazaProductieNoua) throws Exception{
+		try{
+			if (dummyFazaProductieNoua.getIdFazaProductie() == null || 
+				entityManager.find(dummyFazaProductieNoua.getClass(), dummyFazaProductieNoua.getIdFazaProductie()) == null)
+				entityManager.persist(dummyFazaProductieNoua);
+			else
+				entityManager.merge(dummyFazaProductieNoua);
+			
+		}catch(Exception ex){
+			
+			logger.info("EROARE PERSISTENTA ***** ");
+			ex.printStackTrace();
+			throw ex;
+		}
+		return dummyFazaProductieNoua;	}
+
+	
+
+	
+
+	
 }
