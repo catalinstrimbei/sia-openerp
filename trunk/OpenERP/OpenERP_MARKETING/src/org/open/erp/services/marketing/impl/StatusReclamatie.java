f628e229db7b3b27f831e64faf25b39835e835fa
loredana.iuliana.vasile@gmail.com <loredana.iuliana.vasile@gmail.com@01a5fee1-9287-8e58-6871-dad33e70e5c6>
2012-11-17 14:17:40 +0000

    Implemented enums

--- trunk/OpenERP/OpenERP_MARKETING/src/org/open/erp/services/marketing/impl/StatusReclamatie.java
+++ trunk/OpenERP/OpenERP_MARKETING/src/org/open/erp/services/marketing/impl/StatusReclamatie.java
@@ -1,5 +1,23 @@
 package org.open.erp.services.marketing.impl;
 
 public enum StatusReclamatie {
+	
+	NOU("Nou"), IN_ASTEPTARE("In asteptare"), REZOLVAT("Rezolvat");
+
+	private String value;
+	
+	StatusReclamatie(String value) {
+        this.value = value;
+    }
+
+	public String getValue() {
+		return value;
+	}
+	
+	@Override
+	public String toString() {
+		return this.getValue();
+	}
+
 
 }
--- trunk/OpenERP/OpenERP_MARKETING/src/org/open/erp/services/marketing/impl/TipPromovare.java
+++ trunk/OpenERP/OpenERP_MARKETING/src/org/open/erp/services/marketing/impl/TipPromovare.java
@@ -1,5 +1,23 @@
 package org.open.erp.services.marketing.impl;
 
 public enum TipPromovare {
+	
+	RECLAMA("Reclama"), PROMOTIE("Promotie"), FLYERE("Flyere");
+
+	private String value;
+	
+	TipPromovare(String value) {
+        this.value = value;
+    }
+
+	public String getValue() {
+		return value;
+	}
+	
+	@Override
+	public String toString() {
+		return this.getValue();
+	}
+
 
 }
