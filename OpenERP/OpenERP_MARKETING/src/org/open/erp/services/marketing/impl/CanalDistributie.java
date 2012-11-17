d5e02d3ff12d4ab375d85615f3e8b4bdc508d57a
loredana.iuliana.vasile <loredana.iuliana.vasile@gmail.com>
2012-11-17 14:17:40 +0000

    Implemented enums

--- OpenERP/OpenERP_MARKETING/src/org/open/erp/services/marketing/impl/CanalDistributie.java
+++ OpenERP/OpenERP_MARKETING/src/org/open/erp/services/marketing/impl/CanalDistributie.java
@@ -1,5 +1,21 @@
 package org.open.erp.services.marketing.impl;
 
 public enum CanalDistributie {
+	TV("Tv"), RADIO("Radio"), INTERNET("Internet"), PRESA_SCRISA("Presa scrisa");
+
+	private String value;
+	
+	CanalDistributie(String value) {
+        this.value = value;
+    }
+
+	public String getValue() {
+		return value;
+	}
+	
+	@Override
+	public String toString() {
+		return this.getValue();
+	}
 
 }
