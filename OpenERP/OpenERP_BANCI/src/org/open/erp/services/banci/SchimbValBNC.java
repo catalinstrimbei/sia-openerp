0aa432e7e5dadd8a0500e079a9bcca88f1b41e58
vasilica.alupoaie2010 <vasilica.alupoaie2010@gmail.com>
2012-11-18 22:41:18 +0000

    banci

---
+++ OpenERP/OpenERP_BANCI/src/org/open/erp/services/banci/SchimbValBNC.java
@@ -0,0 +1,189 @@
+package org.open.erp.services.banci;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SchimbValBNC {
+
+	@Test
+	public void testTestTestObject() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testTestTestGetClass() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testTestTestHashCode() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testTestTestEquals() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testTestTestClone() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testTestTestToString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testTestTestNotify() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testTestTestNotifyAll() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testTestTestWaitLong() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testTestTestWaitLongInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testTestTestWait() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testTestTestFinalize() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testTestObject() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testTestGetClass() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testTestHashCode() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testTestEquals() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testTestClone() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testTestToString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testTestNotify() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testTestNotifyAll() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testTestWaitLong() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testTestWaitLongInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testTestWait() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testTestFinalize() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testObject() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetClass() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testHashCode() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEquals() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testClone() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testToString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testNotify() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testNotifyAll() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testWaitLong() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testWaitLongInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testWait() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFinalize() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ OpenERP/OpenERP_BANCI/src/org/open/erp/services/nomgen/Clienti.java
@@ -0,0 +1,14 @@
+package org.open.erp.services.nomgen;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Clienti {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
