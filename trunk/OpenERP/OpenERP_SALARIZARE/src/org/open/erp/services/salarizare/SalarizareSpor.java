648de21c6c36cdd71fd77d831153794bb13e54e8
Bragelone <Bragelone@01a5fee1-9287-8e58-6871-dad33e70e5c6>
2012-11-25 18:37:17 +0000

    Salarizare

--- trunk/OpenERP/OpenERP_SALARIZARE/src/org/open/erp/services/salarizare/SalarizareSpor.java
+++ trunk/OpenERP/OpenERP_SALARIZARE/src/org/open/erp/services/salarizare/SalarizareSpor.java
@@ -1,11 +1,49 @@
 package org.open.erp.services.salarizare;
 
 public class SalarizareSpor {
-	
-	private Integer codspor;
-	private String tipspor;
-	private String modcalcul;
-	private Double marimespor;
-	private Date dataSpor;
+	Integer codspor;
+	String tipspor;
+	String modcalcul;
+	Date dataSpor;
+	Double marimespor;
+		
+	public Integer getcodspor() {
+		return codspor;
+	}
 
-}
+	public void setcodspor(Integer codspor) {
+		this.codspor = codspor;
+	}
+
+	public String gettipspor() {
+		return tipspor;
+	}
+
+	public void settipspor(String tipspor) {
+		this.tipspor = tipspor;
+	}
+
+	public String getmodcalcul() {
+		return modcalcul;
+	}
+
+	public void setmodcalcul(String modcalcul) {
+		this.modcalcul = modcalcul;
+	}
+	
+	public Date getdataSpor() {
+		return dataSpor;
+	}
+
+	public void setdataSpor(Date dataSpor) {
+		this.dataSpor = dataSpor;
+	}
+	
+		public Double getmarimespor() {
+		return marimespor;
+	}
+
+
+	public void setmarimespor(Double marimespor) {
+		this.marimespor = marimespor;
+	}
