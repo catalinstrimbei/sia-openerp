6a67a15ccde97143df970ce1f37ee358a0216b8b
Bragelone <Bragelone@01a5fee1-9287-8e58-6871-dad33e70e5c6>
2012-11-25 17:30:54 +0000

    Salarizare Nicolau Vlad Commit

---
+++ trunk/OpenERP/OpenERP_SALARIZARE/src/org/open/erp/services/salarizare/SalarizareSpor.java
@@ -0,0 +1,11 @@
+package org.open.erp.services.salarizare;
+
+public class SalarizareSpor {
+	
+	private Integer codspor;
+	private String tipspor;
+	private String modcalcul;
+	private Double marimespor;
+	private Date dataSpor;
+
+}
