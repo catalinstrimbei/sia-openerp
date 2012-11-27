2f446a3eb646887d615f74d663ddb5fe2f46b14f
vasilica.alupoaie2010 <vasilica.alupoaie2010@gmail.com>
2012-11-27 22:07:13 +0000

    Banci ERP- versiune test

--- OpenERP/OpenERP_BANCI/src/org/open/erp/services/banci/BanciSrv.java
+++ OpenERP/OpenERP_BANCI/src/org/open/erp/services/banci/BanciSrv.java
@@ -1,5 +1,8 @@
 package org.open.erp.services.banci;
 
-public class BanciSrv {
+import java.util.Date;
+import java.util.List;
 
+public interface BanciSrv {
+	public SchimbValBNC creareSchimbValBNC(Date datatranz,Integer monedacurenta,Double valmoncurent,Integer monedaschimb,Double valmondupaschimb,Integer cursval,Double comisionscb,List<LiniiPlati> liniePlata);
 }
