0b77315e46e3c9a2503e8044bf4e33d881495da7
alexandru.morosanu <alexandru.morosanu@gmail.com>
2012-12-08 18:07:02 +0000

    metode get() set() celelalte clase

--- OpenERP/OpenERP_ADMINSECURITATE/src/org/open/erp/services/admin/RolUtilizatori.java
+++ OpenERP/OpenERP_ADMINSECURITATE/src/org/open/erp/services/admin/RolUtilizatori.java
@@ -2,8 +2,43 @@ package org.open.erp.services.admin;
 
 public class RolUtilizatori {
 
-	Integer idRol;
-	String denumireRol;
-	String observatie;
+	Integer IdRol;
+	String DenumireRol;
+	String Observatie;
 	
+	
+	public Integer getIdRol() {
+		return IdRol;
+	}
+	
+	public void setIdRol(Integer IdRol) {
+		this.IdRol = IdRol;
+	}
+	
+	public String getDenumireRol() {
+		return DenumireRol;
+	}
+	
+	public void setDenumireRol(String DenumireRol) {
+		this.DenumireRol = DenumireRol;
+	}
+	
+	public String getObservatie() {
+		return Observatie;
+	}
+	
+	public void setObservatie() {
+		this.Observatie = Observatie;
+	}
+	
+	public RolUtilizatori(Integer IdRol, String DenumireRol, String Observatie) {
+		super();
+		this.IdRol = IdRol;
+		this.DenumireRol = DenumireRol;
+		this.Observatie = Observatie;
+	}
+	
+	public RolUtilizatori() {
+		super();
+	}
 }
