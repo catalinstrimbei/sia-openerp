4c360fbed157bf8c8360c8bb24eecce3168b03a1
alexandru.morosanu <alexandru.morosanu@gmail.com>
2012-12-08 17:46:42 +0000

    metode get set clasa Utilizator

--- OpenERP/OpenERP_ADMINSECURITATE/src/org/open/erp/services/admin/Utilizator.java
+++ OpenERP/OpenERP_ADMINSECURITATE/src/org/open/erp/services/admin/Utilizator.java
@@ -1,16 +1,79 @@
 package org.open.erp.services.admin;
 
+import org.open.erp.services.personal.Angajat;
 
-import org.open.erp.personal.Angajat;
+
+
 
 public class Utilizator extends Angajat{
 
-	Integer id;
-	String functia;
-	String utilizator;
-	String parola;
+	Integer IdUtilizator;
+	String FunctiaUtilizator;
+	String Utilizator;
+	String ParolaUtilizator;
 	Integer idGrup;
 	Integer idRol;
 	
+	public Integer getIdUtilizator(){
+		return IdUtilizator;	
+	}
 	
+	public void setIdUtilizator(Integer IdUtilizator){
+		this.IdUtilizator = IdUtilizator;
+	}
+	
+	public String getFunctiaUtilizator(){
+		return FunctiaUtilizator;
+	}
+	
+	public void setFunctiaUtilizator(String FunctiaUtilizator){
+		this.FunctiaUtilizator = FunctiaUtilizator;
+	}
+	
+	public String getUtilizator() {
+		return Utilizator;
+	}
+	
+	public void setUtilizator(String  Utilizator) {
+		this.Utilizator = Utilizator;
+	}
+	
+	public String getParolaUtilizator() {
+		return ParolaUtilizator;
+	}
+	
+	public void setParolaUtilizator(String ParolaUtilizator) {
+		this.ParolaUtilizator = ParolaUtilizator;
+	}
+	
+	public Integer getidGrup() {
+		return idGrup;
+	}
+	
+	public void setidGrup(Integer idGrup) {
+		this.idGrup = idGrup;
+	}
+	
+	public Integer getidRol() {
+		return idRol;
+	}
+	
+	public void setidRol(Integer idRol) {
+		this.idRol = idRol;
+	}
+	
+	
+	public Utilizator (Integer IdUtilizator, String FunctiaUtilizator, String Utilizator, String ParolaUtilizator, Integer idGrup, Integer idRol) {
+		super();
+		this.IdUtilizator = IdUtilizator;
+		this.FunctiaUtilizator = FunctiaUtilizator;
+		this.Utilizator = Utilizator;
+		this.ParolaUtilizator = ParolaUtilizator;
+		this.idGrup = idGrup;
+		this.idRol = idRol;
+	}
+	
+	public Utilizator() {
+		super();
+	}
 }
