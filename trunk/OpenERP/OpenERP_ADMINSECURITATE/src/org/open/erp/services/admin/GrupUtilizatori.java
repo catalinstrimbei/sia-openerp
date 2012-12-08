129911413eca146862c2a66a16a6e90e6213fcbd
alexandru.morosanu@gmail.com <alexandru.morosanu@gmail.com@01a5fee1-9287-8e58-6871-dad33e70e5c6>
2012-12-08 18:07:02 +0000

    metode get() set() celelalte clase

--- trunk/OpenERP/OpenERP_ADMINSECURITATE/src/org/open/erp/services/admin/GrupUtilizatori.java
+++ trunk/OpenERP/OpenERP_ADMINSECURITATE/src/org/open/erp/services/admin/GrupUtilizatori.java
@@ -2,8 +2,52 @@ package org.open.erp.services.admin;
 
 public class GrupUtilizatori {
 
-	Integer idGrup;
-	String denumireGrup;
-	String observatie;
-	Integer idDrept;
+	Integer IdGrup;
+	String DenumireGrup;
+	String Observatie;
+	Integer IdDrept;
+	
+	public Integer getIdGrup() {
+		return IdGrup;
+	}
+	
+	public void setIdGrup(Integer IdGrup) {
+		this.IdGrup = IdGrup;
+	}
+	
+	public String DenumireGrup() {
+		return DenumireGrup;
+	}
+	
+	public void setDenumireGrup(String DenumireGrup) {
+		this.DenumireGrup = DenumireGrup;
+	}
+	
+	public String getObservatie() {
+		return Observatie;
+	} 
+	
+	public void setObservatie( String Observatie) {
+		this.Observatie = Observatie;
+	}
+	
+	public Integer getIdDrept() {
+		return IdDrept;
+	}
+	
+	public void setIdDrept(Integer IdDrept) {
+		this.IdDrept = IdDrept;
+	}
+	
+	public GrupUtilizatori(Integer IdGrup, String DenumireGrup, String Observatie, Integer IdDrept) {
+		super();
+		this.IdGrup = IdGrup;
+		this.DenumireGrup = DenumireGrup;
+		this.Observatie = Observatie;
+		this.IdDrept = IdDrept;
+	}
+	
+	public GrupUtilizatori() {
+		super();
+	}
 }
