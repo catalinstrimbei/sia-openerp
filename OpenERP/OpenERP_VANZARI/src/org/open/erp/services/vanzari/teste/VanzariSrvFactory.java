e9e25f663dbd470cd2072b3584901f9f47d12d82
mihalescu.ionela <mihalescu.ionela@gmail.com>
2012-12-09 19:05:30 +0000

    [UPDATED] Fara DUMMY PERSONAL

--- OpenERP/OpenERP_VANZARI/src/org/open/erp/services/vanzari/teste/VanzariSrvFactory.java
+++ OpenERP/OpenERP_VANZARI/src/org/open/erp/services/vanzari/teste/VanzariSrvFactory.java
@@ -1,12 +1,12 @@
 package org.open.erp.services.vanzari.teste;
 
+import java.util.Date;
+import java.util.List;
+
 import org.apache.log4j.Logger;
 
 import org.open.erp.services.nommat.Material;
-import org.open.erp.services.personal.PersonalSrv;
 import org.open.erp.services.stocuri.StocuriSrv;
-import org.open.erp.services.vanzari.Produse;
-import org.open.erp.services.vanzari.Responsabil;
 import org.open.erp.services.vanzari.VanzariSrv;
 import org.open.erp.services.vanzari.impl.VanzariImpl;
 
@@ -17,31 +17,12 @@ public class VanzariSrvFactory {
 	public static VanzariSrv getVanzariSrv(){
 		VanzariSrv vanzari = new VanzariImpl();
 		logger.info("Creare instanta VanzariSRV din VanzariSrvFactory!");
-		PersonalSrv personalSrv=VanzariSrvFactory.getPersonalSrv();
 		StocuriSrv stocuriSrv=VanzariSrvFactory.getStocuriSrv();
-		vanzari.setPersonalSrv(personalSrv);
 		vanzari.setStocuri(stocuriSrv);
-				
 		return vanzari;
 		
 	}
 	
-	public static PersonalSrv getPersonalSrv(){
-		logger.info("Creaza Dummy PERSONAL SRV----");
-		return new PersonalSrv()
-		{
-
-
-			@Override
-			public Responsabil preluarePersoana(String ocupatie) {
-				logger.info("5.1 a) Am delegat persoana responsabila");
-				return new Responsabil();
-			}
-			
-			
-		};
-	}
-
 	
 	
 	public static StocuriSrv getStocuriSrv(){
@@ -61,10 +42,7 @@ public class VanzariSrvFactory {
 				
 				
 			}
-
-			
-			
-			
+		
 		};
 	}
 }
