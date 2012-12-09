d5cf2d236f57ee9ac3174e7c7a37d4df29a80558
vasilica.alupoaie2010 <vasilica.alupoaie2010@gmail.com>
2012-12-09 19:04:49 +0000

    bnk11

--- OpenERP/OpenERP_BANCI/src/org/open/erp/services/banci/BanciSrv.java
+++ OpenERP/OpenERP_BANCI/src/org/open/erp/services/banci/BanciSrv.java
@@ -10,6 +10,34 @@ public interface BanciSrv {
 	public void sumeRetrase(CarduriBNC card);
 	
 	
+	/* Obtinerea unui raport cu ce depozite are o companie si la ce banci?
+	 * 	Suma depusa initiala in contul ca economie
+	2.	Tip cont(lei valuta)
+	3.	Perioada depozit
+	4.	Suma aflata in cont la momentul actual.
+	5.  Cand si ce sume am depus.
+	6. Cand si sume am retras.
+	 
+	 /* un Raport cu conturile inchise 
+	  
+	  nume cont
+	  titular(companie)
+	  data deschiderii cont 
+	  data inchiderii cont
+	  motiv inchidere
+	  rulaj cont sa vedem ce sume s-au tranzactionat in acel cont pentru a intelege daca am pierdut un client bun inainte de inchidere.
+	  */
+	 
+	 
+	/*Obtinerea unui raport cu conturile inca deschise
+	 *  nume cont
+	  titular(companie)
+	  data deschiderii cont 
+	  rulaj cont sa vedem ce sume s-au tranzactionat in acel cont pana in prezent pt a intelege daca clientul este important sau nu.
+	  */
+	 */
+	 
+	
 	/*
 	1.	Obtinerea unui extras de cont pentru un cont anume la o banca cu care lucreaza o companie
 	2.	Obtinerea unui raport cu ce credite unei companii si la ce banci
