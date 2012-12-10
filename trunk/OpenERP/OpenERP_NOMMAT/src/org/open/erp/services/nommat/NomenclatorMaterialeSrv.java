272d96f7b5ce04c633528f7fb2c531434f6c045b
cristina.ionescu.c@gmail.com <cristina.ionescu.c@gmail.com@01a5fee1-9287-8e58-6871-dad33e70e5c6>
2012-12-10 12:29:29 +0000

    E14 NOM MAT Cristina Ionescu Adaugare de teste

--- trunk/OpenERP/OpenERP_NOMMAT/src/org/open/erp/services/nommat/NomenclatorMaterialeSrv.java
+++ trunk/OpenERP/OpenERP_NOMMAT/src/org/open/erp/services/nommat/NomenclatorMaterialeSrv.java
@@ -1,8 +1,11 @@
 package org.open.erp.services.nommat;
 
+import org.open.erp.services.nommat.Material;
+
 public interface NomenclatorMaterialeSrv {
 	 Material introducereMaterial(String codMaterial, String denumireMaterial, String cantitateStandard, String pretStandard,
 			 	String categorieMaterial, String procentTVACurent, String observatii, ListaCaracteristici listaCaracteristici);
+	 Material cautareMaterialDupaDenumire(String denumire);
 	 UnitateDeMasura incarcareUnitate(String id, String unitateDeMasura);
 	 ListaCaracteristici incarcareLista(String id, String listaCaracteristici);
 	}
