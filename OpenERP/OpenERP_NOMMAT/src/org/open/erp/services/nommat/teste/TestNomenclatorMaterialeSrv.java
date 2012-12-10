01b545a388260749b506d78126d0289d1659e15f
cristina.ionescu.c <cristina.ionescu.c@gmail.com>
2012-12-10 12:29:29 +0000

    E14 NOM MAT Cristina Ionescu Adaugare de teste

--- OpenERP/OpenERP_NOMMAT/src/org/open/erp/services/nommat/teste/TestNomenclatorMaterialeSrv.java
+++ OpenERP/OpenERP_NOMMAT/src/org/open/erp/services/nommat/teste/TestNomenclatorMaterialeSrv.java
@@ -7,6 +7,8 @@ import org.junit.Test;
 import org.open.erp.services.nommat.ListaCaracteristici;
 import org.open.erp.services.nommat.Material;
 import org.open.erp.services.nommat.NomenclatorMaterialeSrv;
+import org.open.erp.services.nommat.teste.NomenclatorMaterialeSrvFactory;
+import org.open.erp.services.nommat.teste.TestNomenclatorMaterialeSrv;
 
 
 public class TestNomenclatorMaterialeSrv {
@@ -27,7 +29,7 @@ public class TestNomenclatorMaterialeSrv {
 	@Test
 	public void testIntroducereMaterial() throws Exception{
 		ListaCaracteristici caract = nomenInstance.incarcareLista("1", "Material Dur");
-		Material mat = nomenInstance.introducereMaterial("1", "fier", "20", "5"," 1.2", null, caract);
+		Material mat = nomenInstance.introducereMaterial("1", "fier", "20", "5"," 1.2", null, "materie prima", caract);
 		logger.debug("++++++ Afisare Material: " + mat.getDenumireMaterial() + "; "+ "Detalii: " + mat.getPretStandard() + ", " + mat.getCantitateStandard());
 		logger.debug("++++ Afisare ListaCaract: " + caract.getId() + " " + caract.getListaCaracteristici());
 		
@@ -35,12 +37,12 @@ public class TestNomenclatorMaterialeSrv {
 	
 	@Test
 	public void testCautareMaterial() throws Exception{
-		ListaCaracteristici caract = nomenInstance.incarcareLista("2", "Material Fin");
-		Material mat = nomenInstance.introducereMaterial("2", "Canepa", "20", "5"," 1.2", null, caract);
-		
-		logger.debug("Cautare material Canepa:" + nomenInstance.cautareMaterialDupaDenumire);
-		
-	}
+		  ListaCaracteristici caract = nomenInstance.incarcareLista("2", "Material Fin");
+		  Material mat = nomenInstance.introducereMaterial("2", "Canepa", "20", "5"," 1.2", null,"materie prima", caract);
+		  String denumire= "Canepa";
+		  logger.debug("Cautare material Canepa:" + nomenInstance.cautareMaterialDupaDenumire(denumire));
+		  
+		 }
    
    
 }
