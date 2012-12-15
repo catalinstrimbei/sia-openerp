0371645bcd6b8febf673a0795b6bcca263bc218f
catalin.baciu89@gmail.com <catalin.baciu89@gmail.com@01a5fee1-9287-8e58-6871-dad33e70e5c6>
2012-12-15 16:34:38 +0000

    Echipa 14 - Productie Scopul comiterii: pentru a elimina dependentele dummy ale modulului nostru (Productie), am creat in modulul PERSONAL clasa Functie.java

---
+++ trunk/OpenERP/OpenERP_PERSONAL/src/org/open/erp/services/personal/Functie.java
@@ -0,0 +1,71 @@
+package org.open.erp.services.personal;
+
+import java.io.Serializable;
+
+public class Functie implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private Integer 		idFunctie;
+	private String 			numeFunctie;
+	
+	public Integer getIdFunctie() {
+		return idFunctie;
+	}
+	public void setIdFunctie(Integer idFunctie) {
+		this.idFunctie = idFunctie;
+	}
+	public String getNumeFunctie() {
+		return numeFunctie;
+	}
+	public void setNumeFunctie(String numeFunctie) {
+		this.numeFunctie = numeFunctie;
+	}
+	
+	public Functie(Integer idFunctie, String numeFunctie) {
+		super();
+		this.idFunctie = idFunctie;
+		this.numeFunctie = numeFunctie;
+	}
+	
+	public Functie() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result
+				+ ((idFunctie == null) ? 0 : idFunctie.hashCode());
+		result = prime * result
+				+ ((numeFunctie == null) ? 0 : numeFunctie.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Functie other = (Functie) obj;
+		if (idFunctie == null) {
+			if (other.idFunctie != null)
+				return false;
+		} else if (!idFunctie.equals(other.idFunctie))
+			return false;
+		if (numeFunctie == null) {
+			if (other.numeFunctie != null)
+				return false;
+		} else if (!numeFunctie.equals(other.numeFunctie))
+			return false;
+		return true;
+	}
+	
+	
+}
