132c32b6078b7204e14a876bcf052bb1445e4953
mihalescu.ionela@gmail.com <mihalescu.ionela@gmail.com@01a5fee1-9287-8e58-6871-dad33e70e5c6>
2013-01-27 14:55:29 +0000

    test

--- trunk/OpenERP/OpenERP_TestSuite/appClientModule/org/open/erp/services/vanzari/teste/TestVanzariEJB.java
+++ trunk/OpenERP/OpenERP_TestSuite/appClientModule/org/open/erp/services/vanzari/teste/TestVanzariEJB.java
@@ -1,61 +1,40 @@
 package org.open.erp.services.vanzari.teste;
 
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Date;
-
 import org.apache.log4j.Logger;
-import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
-import org.open.erp.services.nomgen.Adresa;
-import org.open.erp.services.nomgen.PersoanaFizica;
-import org.open.erp.services.nommat.ListaCaracteristici;
-import org.open.erp.services.nommat.Material;
-import org.open.erp.services.personal.Angajat;
-import org.open.erp.services.stocuri.Depozit;
-import org.open.erp.services.stocuri.Gestiune;
-import org.open.erp.services.stocuri.StocuriSrv;
-import org.open.erp.services.stocuri.teste.StocuriSrvFactory;
-import org.open.erp.services.stocuri.teste.TestStocuriEJB;
-import org.open.erp.services.vanzari.ArticolComanda;
-import org.open.erp.services.vanzari.Avize;
-import org.open.erp.services.vanzari.Clienti;
-import org.open.erp.services.vanzari.Comenzi;
-import org.open.erp.services.vanzari.DispozitiiLivrare;
-import org.open.erp.services.vanzari.Facturi;
-import org.open.erp.services.vanzari.LiniiAviz;
-import org.open.erp.services.vanzari.LiniiDispozitieLivrare;
-import org.open.erp.services.vanzari.LiniiFactura;
-import org.open.erp.services.vanzari.OfertePret;
 import org.open.erp.services.vanzari.Persoana;
-import org.open.erp.services.vanzari.Produse;
 import org.open.erp.services.vanzari.VanzariSrv;
 
 public class TestVanzariEJB {
-	// Resurse test
-		private static Logger logger = Logger.getLogger(TestVanzariEJB.class.getName());
-		
-		// Unitatea de test sursa/gazda unitatii de test 
-		public static VanzariSrv vanzariInstance;
-		
-		// Set up 
-		@BeforeClass
-		public static void setUpBeforeClass() throws Exception {
-			vanzariInstance = VanzariSrvFactory.getVanzariSrv();
-			logger.info("initTest " + vanzariInstance);
-		}
-		
-		@Test
-		public void testCrearePersoana() throws Exception{
-			logger.info("-----START creare persoana----- ");
-			
-			Persoana persoana = vanzariInstance.crearePersoana(1, "ION", "Ion", "sofer");
-			
-		
-			logger.info("-----SFARSIT creare persoana----- " + persoana.getNume() );
-			
-		}
-		
 	
-}
+	
+		// Resurse test
+			private static Logger logger = Logger.getLogger(TestVanzariEJB.class.getName());
+			
+			// Unitatea de test sursa/gazda unitatii de test 
+			public static VanzariSrv vanzariInstance;
+			
+			// Set up 
+			@BeforeClass
+			public static void setUpBeforeClass() throws Exception {
+				vanzariInstance = VanzariSrvFactory.getVanzariSrv();
+				logger.info("initTest " + vanzariInstance);
+			}
+			
+			@Test
+			public void testCrearePersoana() throws Exception{
+				logger.info("-----***START creare persoana----- ");
+				
+				Persoana persoana = vanzariInstance.crearePersoana(1, "ION", "Ion", "sofer");
+				
+			
+				logger.info("-----SFARSIT creare persoana----- " + persoana.getNume() );
+				
+			}
+			
+		
+	}
+
+
+
--- trunk/OpenERP/OpenERP_TestSuite/appClientModule/org/open/erp/services/vanzari/teste/VanzariSrvFactory.java
+++ trunk/OpenERP/OpenERP_TestSuite/appClientModule/org/open/erp/services/vanzari/teste/VanzariSrvFactory.java
@@ -1,27 +1,28 @@
 package org.open.erp.services.vanzari.teste;
 
 import java.util.Hashtable;
+
 import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
+
 import org.apache.log4j.Logger;
-import org.open.erp.services.stocuri.StocuriSrv;
-import org.open.erp.services.stocuri.teste.StocuriSrvFactory;
 import org.open.erp.services.vanzari.VanzariSrv;
 
 public class VanzariSrvFactory {
-private static Logger logger = Logger.getLogger(VanzariSrvFactory.class.getName());
-	
-	public static VanzariSrv getVanzariSrv() throws Exception{
-		return (VanzariSrv) lookupEJBService();
-	}
 
-	private static <T> T lookupEJBService() throws NamingException {
-		final Hashtable jndiProperties = new Hashtable();
-        jndiProperties.put(Context.URL_PKG_PREFIXES, "org.jboss.ejb.client.naming");
-        logger.info(Context.URL_PKG_PREFIXES);
-        final Context context = new InitialContext(jndiProperties);
-        logger.info("LOOKUP FOR OpenERP_VANZARI/VanzariImpl!org.open.erp.services.vanzari.VanzariSrv");
-        return (T) context.lookup("ejb:/OpenERP_VANZARI/VanzariImpl!org.open.erp.services.vanzari.VanzariSrv");
-	}	
-}
+	private static Logger logger = Logger.getLogger(VanzariSrvFactory.class.getName());
+		
+		public static VanzariSrv getVanzariSrv() throws Exception{
+			return (VanzariSrv) lookupEJBService();
+		}
+
+		private static <T> T lookupEJBService() throws NamingException {
+			final Hashtable jndiProperties = new Hashtable();
+	        jndiProperties.put(Context.URL_PKG_PREFIXES, "org.jboss.ejb.client.naming");
+	        logger.info(Context.URL_PKG_PREFIXES);
+	        final Context context = new InitialContext(jndiProperties);
+	        logger.info("LOOKUP FOR OpenERP_VANZARI/VanzariImpl!org.open.erp.services.vanzari.VanzariSrv");
+	        return (T) context.lookup("ejb:/OpenERP_VANZARI/VanzariImpl!org.open.erp.services.vanzari.VanzariSrv");
+		}	
+	}
