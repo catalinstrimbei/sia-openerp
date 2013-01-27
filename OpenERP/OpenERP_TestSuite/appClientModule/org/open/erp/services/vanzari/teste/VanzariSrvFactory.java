74f1c90af07467fd10c3a82a15091065f78f9d2a
mihalescu.ionela <mihalescu.ionela@gmail.com>
2013-01-27 13:46:57 +0000

    Test

--- OpenERP/OpenERP_TestSuite/appClientModule/org/open/erp/services/vanzari/teste/VanzariSrvFactory.java
+++ OpenERP/OpenERP_TestSuite/appClientModule/org/open/erp/services/vanzari/teste/VanzariSrvFactory.java
@@ -5,6 +5,8 @@ import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
 import org.apache.log4j.Logger;
+import org.open.erp.services.stocuri.StocuriSrv;
+import org.open.erp.services.stocuri.teste.StocuriSrvFactory;
 import org.open.erp.services.vanzari.VanzariSrv;
 
 public class VanzariSrvFactory {
@@ -13,13 +15,13 @@ private static Logger logger = Logger.getLogger(VanzariSrvFactory.class.getName(
 	public static VanzariSrv getVanzariSrv() throws Exception{
 		return (VanzariSrv) lookupEJBService();
 	}
-	
-    private static <T> T lookupEJBService() throws NamingException {
-        final Hashtable jndiProperties = new Hashtable();
+
+	private static <T> T lookupEJBService() throws NamingException {
+		final Hashtable jndiProperties = new Hashtable();
         jndiProperties.put(Context.URL_PKG_PREFIXES, "org.jboss.ejb.client.naming");
         logger.info(Context.URL_PKG_PREFIXES);
         final Context context = new InitialContext(jndiProperties);
         logger.info("LOOKUP FOR OpenERP_VANZARI/VanzariImpl!org.open.erp.services.vanzari.VanzariSrv");
         return (T) context.lookup("ejb:/OpenERP_VANZARI/VanzariImpl!org.open.erp.services.vanzari.VanzariSrv");
-    }	
+	}	
 }
