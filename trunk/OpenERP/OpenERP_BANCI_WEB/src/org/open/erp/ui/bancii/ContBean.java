3579d556fd79de39eafa0392f18ab8217915ca7b
paraschivgeanina <paraschivgeanina@01a5fee1-9287-8e58-6871-dad33e70e5c6>
2013-01-28 11:16:06 +0000

    modulul banci web

--- trunk/OpenERP/OpenERP_BANCI_WEB/src/org/open/erp/ui/bancii/ContBean.java
+++ trunk/OpenERP/OpenERP_BANCI_WEB/src/org/open/erp/ui/bancii/ContBean.java
@@ -1,23 +1,17 @@
 package org.open.erp.ui.bancii;
 
-import java.io.Serializable;
-
 import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
 
 import org.open.erp.services.banci.BanciSrv;
 
-
-@ManagedBean(name = "carduribncBean")
+@ManagedBean(name = "contBean")
 @javax.faces.bean.RequestScoped
-public class CarduriBNCBean implements Serializable {
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = -2577929799400568953L;
-	
+public class ContBean {
 	@EJB(lookup="java:global/OpenERP_BANCI/BanciImpl!org.open.erp.services.banci.BanciSrv")
 	private BanciSrv banciSrv;
+	
+
 
 	public BanciSrv getBanciSrv() {
 		return banciSrv;
@@ -26,6 +20,4 @@ public class CarduriBNCBean implements Serializable {
 	public void setBanciSrv(BanciSrv banciSrv) {
 		this.banciSrv = banciSrv;
 	}
-	
-
 }
