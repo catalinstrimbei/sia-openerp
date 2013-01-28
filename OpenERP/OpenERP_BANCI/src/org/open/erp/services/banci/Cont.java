87928a8c88f130f09cb840a8a3feb728636ee310
paraschivgeanina <paraschivgeanina@01a5fee1-9287-8e58-6871-dad33e70e5c6>
2013-01-28 10:40:10 +0000

    banci

--- OpenERP/OpenERP_BANCI/src/org/open/erp/services/banci/Cont.java
+++ OpenERP/OpenERP_BANCI/src/org/open/erp/services/banci/Cont.java
@@ -1,6 +1,14 @@
 package org.open.erp.services.banci;
 
-public class Cont{
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Cont implements Serializable{
+	@Id @GeneratedValue
 	Integer id;
 	String nume;
 	String tipCont;
